8f3980b7062aa8f3650ef6706fd378d5731354f2
daephx <36192863+daephx@users.noreply.github.com>
2021-11-02 01:11:34 -0700

    feat: init function for colorscheme settings
    Some colorschemes come with diffrent styles and settings;
    this function checks for module files in lua/colors that match a provided colorscheme name.
    If the file exists it will source it and attempt to execute an override function,
    where you can over write highlight groups to fit your preference

---
+++ lua/colors/init.lua
@@ -0,0 +1,52 @@
+--    ______      __
+--   / ____/___  / /___  __________
+--  / /   / __ \/ / __ \/ ___/ ___/
+-- / /___/ /_/ / / /_/ / /  (__  )
+-- \____/\____/_/\____/_/  /____/
+-- ================================
+-- init module for loading colorschemes
+-- and overrides for common highlight groups
+
+local api = vim.api
+local cmd = vim.cmd
+local g = vim.g
+
+-- local highlight = vim.api.nvim_set_hl
+
+local M = {}
+
+function M.setup(name)
+  local ok, colorscheme = pcall(require, "colors."..name)
+  if not ok or name ~= g.colors_name then
+    return
+  end
+
+  -- Load colorscheme
+  cmd('colorscheme ' .. name)
+
+  -- Load colorscheme specific overrides
+  colorscheme.overrides()
+
+  -- All highlight overrides MUST be executed after colorscheme Initialization
+  local overrides = require('colors.overrides')
+  overrides.GeneralOverrides() -- Unstructed highlight tweaks
+  overrides.TransparentBackground() -- Disable 'Normal' higroup
+
+  api.nvim_echo(
+    {{'Loaded Colorscheme: ', 'debug'}, {name, 'None'}}, true, {}
+  )
+
+  -- Define colorscheme autogroup
+  -- Reload this function when colorscheme is updated
+
+  cmd [[
+  augroup colorscheme_au
+    autocmd!
+    autocmd ColorScheme *
+      \ lua require('colors').setup(vim.g.colors_name)
+  augroup end
+  ]]
+
+end
+
+return M

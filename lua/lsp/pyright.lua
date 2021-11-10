d5f29dea20caeb4baf5442fc7867fc40afd0cf6d
daephx <36192863+daephx@users.noreply.github.com>
2021-11-10 13:42:58 -0800

    feat(lsp): add basic config for pyright lsp
    Not much in the way of personal configurations for this module.
    Simply ensures that the default settings are initialized when the lsp
    attaches to the appropriate file type.

---
+++ lua/lsp/pyright.lua
@@ -0,0 +1,14 @@
+-- File: python-lsp.lua
+-- Description: Language-server configuration
+--
+-- Language-server binaries are required for this to work
+-- Some are eeasily to install than others.
+--
+-- For more information on setting up language-servers, See:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
+
+local M = {}
+
+M.config = {}
+
+return M

fa0ba39066139b4a84507ee4b6413c5b7ec0ed0f
daephx <36192863+daephx@users.noreply.github.com>
2021-11-10 13:14:11 -0800

    feat(plugins): add git integration plugins
    Includes 'vim-fugitive', 'gitsigns' and 'neogit'.
    Currently, fugitive is still pretty effective for my workflow so the
    others are just here for style and to checkout the competition
    respectively.

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,44 +1,74 @@
-local neogit = require("neogit")
+-- Neogit | magit for NeoVim
+
+-- Prevent loading if not applicable
+local ok, neogit = pcall(require, "neogit")
+if not ok then
+  return
+end
 
 neogit.setup {
-  disable_signs = false,
-  disable_context_highlighting = false,
-  disable_commit_confirmation = false,
+  kind = { 'tab' },
   auto_refresh = true,
+  disable_signs = false,
+  disable_context_highlighting = true,
+  disable_commit_confirmation = false,
   disable_builtin_notifications = false,
-  commit_popup = {
-      kind = "split",
-  },
-  -- customize displayed signs
-  signs = {
-    -- { CLOSED, OPENED }
-    section = { ">", "v" },
-    item = { ">", "v" },
-    hunk = { "", "" },
-  },
+  use_magit_keybindings = true,
+  commit_popup = { kind = 'split' },
   integrations = {
     -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
     -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
-    --
-    -- Requires you to have `sindrets/diffview.nvim` installed.
-    -- use {
-    --   'TimUntersberger/neogit',
-    --   requires = {
-    --     'nvim-lua/plenary.nvim',
-    --     'sindrets/diffview.nvim'
-    --   }
-    -- }
-    --
-    diffview = true
+    diffview = true -- Requires you to have `sindrets/diffview.nvim` installed.
   },
-  -- override/add mappings
-  mappings = {
-    -- modify status buffer mappings
-    status = {
-      -- Adds a mapping with "B" as key that does the "BranchPopup" command
-      ["B"] = "BranchPopup",
-      -- Removes the default mapping of "s"
-      ["s"] = "",
+  mappings = { -- override/add mappings
+    status = { -- modify status buffer mappings
+      -- Adds a mapping with 'B' as key that does the 'BranchPopup' command
+      ['B'] = 'BranchPopup',
+      ['q'] = 'Close',
+      -- ['s'] = 'Stage',
+      -- ['S'] = '', -- Disable Stage all
     }
-  }
+  },
+  -- Setting any section to `false` will make the section not render at all
+  sections = {
+    untracked = {
+      folded = true
+    },
+    unstaged = {
+      folded = true
+    },
+    staged = {
+      folded = false
+    },
+    stashes = {
+      folded = true
+    },
+    unpulled = {
+      folded = true
+    },
+    unmerged = {
+      folded = false
+    },
+    recent = {
+      folded = false
+    },
+  },
 }
+
+
+vim.cmd [[
+" Highlights
+highlight! link NeogitDiffContextHighlight StatusLineNC
+highlight NeogitCommitViewHeader guibg=none guifg=lightblue
+highlight NeogitObjectId guibg=none guifg=#A056BB
+
+augroup neogit_au
+  autocmd!
+
+  " Disable number column for most window views
+  autocmd FileType NeogitStatus setlocal nonumber | setlocal cursorline
+  autocmd FileType NeogitLogView setlocal nonumber | setlocal cursorline | colorcolumn=
+  autocmd FileType NeogitCommitView setlocal nonumber | setlocal cursorline
+
+augroup END
+]]

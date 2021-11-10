645220de470bc31e2011d16e963c03764a6517a7
daephx <daephxdev@gmail.com>
2021-11-10 13:03:39 -0800

    feat(plugins): add statusline plugin definitions
    Includes lualine settings with a few custom extensions for better
    compatibility with different plugins.

---
+++ lua/lualine/extensions/trouble.lua
@@ -0,0 +1,21 @@
+local function trouble_statusline()
+  return 'Trouble'
+end
+
+local function trouble_itemcount()
+  return #require('trouble').get_items()
+end
+
+local M = {}
+
+M.sections = {
+  lualine_a = { trouble_statusline },
+  -- lualine_b = { trouble_itemcount },
+  lualine_z = { trouble_itemcount, 'location' }
+}
+
+M.inactive_sections = {}
+
+M.filetypes = { 'Trouble' }
+
+return M

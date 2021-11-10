327486644c2eaf60791195d9efb75ddebca1b6d0
daephx <36192863+daephx@users.noreply.github.com>
2021-11-10 13:40:08 -0800

    feat(lsp): add configs for sh/bash language-server
    Whether or not this works with zsh is undetermined from my testing, it
    doesn't appear as if the repo makes any claims to support it but at
    least some of the syntax would work. However, explicitly adding the
    `zsh` extension as a file type did not seem to trigger the server
    attachment.

---
+++ lua/lsp/bashls.lua
@@ -0,0 +1,16 @@
+-- File: bash-lsp.lua
+-- Description: Language-server configuration
+--
+-- Language-server binaries are required for this to work
+-- Some are easily to install than others.
+--
+-- For more information on setting up language-servers, See:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
+
+local M = {}
+
+M.config = {
+  filetypes = {"sh", "zsh", "bash"}
+}
+
+return M

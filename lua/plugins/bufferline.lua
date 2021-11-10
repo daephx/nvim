e047e83006ab14dfbfba71e0fa0d21b0d8e0229b
daephx <daephxdev@gmail.com>
2021-11-10 12:56:54 -0800

    feat(plugins): add tabline plugins
    Includes definitions for tabline plugins.
    Barbar was what I was previously using, tabline.nvim is currently
    enabled.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,12 +1,14 @@
-require('bufferline').setup {
+local bufferline = require('bufferline')
+
+bufferline.setup {
   options = {
-    -- numbers = "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+    -- numbers = 'none' | 'ordinal' | 'buffer_id' | 'both' | function({ ordinal, id, lower, raise }): string,
     --- deprecated, please specify numbers as a function to customize the styling
-    -- number_style = "superscript" | "subscript" | "" | { "none", "subscript" }, -- buffer_id at index 1, ordinal at index 2
-    close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-    left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
+    -- number_style = 'superscript' | 'subscript' | '' | { 'none', 'subscript' }, -- buffer_id at index 1, ordinal at index 2
+    close_command = 'bdelete! %d',       -- can be a string | function, see 'Mouse actions'
+    right_mouse_command = nil, -- can be a string | function, see 'Mouse actions'
+    left_mouse_command = 'buffer %d',    -- can be a string | function, see 'Mouse actions'
+    middle_mouse_command = nil,          -- can be a string | function, see 'Mouse actions'
     -- NOTE: this plugin is designed with this icon in mind,
     -- and so changing this is NOT recommended, this is intended
     -- as an escape hatch for people who cannot bear it for whatever reason
@@ -20,7 +22,7 @@ require('bufferline').setup {
     --- Please note some names can/will break the
     --- bufferline so use this at your discretion knowing that it has
     --- some limitations that will *NOT* be fixed.
-    name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
+    name_formatter = function(buf)  -- buf contains a 'name', 'path' and 'bufnr'
       -- remove extension from markdown files for example
       if buf.name:match('%.md') then
         return vim.fn.fnamemodify(buf.name, ':t:r')
@@ -29,42 +31,40 @@ require('bufferline').setup {
     max_name_length = 18,
     max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
     tab_size = 18,
-    diagnostics = false | "nvim_lsp" | "coc",
-    diagnostics_update_in_insert = false,
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      return "("..count..")"
-    end,
-    -- NOTE: this will be called a lot so don't do any heavy processing here
-    custom_filter = function(buf_number)
-      -- filter out filetypes you don't want to see
-      if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-        return true
-      end
-      -- filter out by buffer name
-      if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-        return true
-      end
-      -- filter out based on arbitrary rules
-      -- e.g. filter out vim wiki buffer from tabline in your work repo
-      if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-        return true
-      end
-    end,
-    offsets = {{filetype = "NvimTree", text = "File Explorer" | function , text_align = "left" | "center" | "right"}},
-    show_buffer_icons = true | false, -- disable filetype icons for buffers
-    show_buffer_close_icons = true | false,
-    show_close_icon = true | false,
-    show_tab_indicators = true | false,
+    diagnostics = false,
+    custom_areas = {},
+    offsets = {{
+      filetype = 'NvimTree',
+      text = 'File Explorer',
+      highlight = 'BufferlineNvimTree',
+      -- highlight = 'Directory',
+      text_align = 'center',
+    }},
+    show_buffer_icons = true, -- disable filetype icons for buffers
+    show_buffer_close_icons = true,
+    show_close_icon = false,
+    show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = "slant" | "thick" | "thin" | { 'any', 'any' },
-    enforce_regular_tabs = false | true,
-    always_show_bufferline = true | false,
-    sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
-      -- add custom logic
-      return buffer_a.modified > buffer_b.modified
-    end
+    separator_style = 'thin',
+    enforce_regular_tabs = false,
+    always_show_bufferline = true,
+    sort_by = 'id',
+  },
+  highlights = {
+    fill = {
+      guibg = {
+        attribute = "bg",
+        highlight = "Pmenu"
+      }
+    },
   }
 }
 
+vim.cmd [[
+  highlight! link BufferlineNvimTree TabLine
+  highlight       BufferlineNvimTree guifg=#e1e1e1
+" highlight! link BufferLineSeparator Constant
+" highlight! link BufferLineSeparatorVisible Constant
+]]

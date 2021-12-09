3db873f08a55abd2ae34c7bc30de1d136445e582
daephx <36192863+daephx@users.noreply.github.com>
2021-12-09 02:29:06 -0800

    feat: definitions for neorg plugins
    Neorg is an attempt at replicating orgmode in lua for neovim.

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,26 @@
+local neorg = require('neorg')
+
+neorg.setup {
+  load = { -- Tell Neorg what modules to load
+    ["core.defaults"] = {}, -- Load all the default modules
+    ["core.keybinds"] = { -- Configure core.keybinds
+      config = {
+        default_keybinds = true, -- Generate the default keybinds
+        neorg_leader = "<Leader>o" -- This is the default if unspecified
+      }
+    },
+    ["core.norg.completion"] = {
+      config = {
+        engine = "nvim-compe" | "nvim-cmp" -- We current support nvim-compe and nvim-cmp only
+      }
+    },
+    ["core.norg.concealer"] = {}, -- Allows for use of icons
+    ["core.norg.dirman"] = { -- Manage your directories with Neorg
+      config = {
+        workspaces = {
+          my_workspace = "~/Documents/neorg"
+        }
+      }
+    },
+  }
+}

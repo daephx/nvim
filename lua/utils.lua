44face45b549efb06e7bd17712d83a1030295937
daephx <daephxdev@gmail.com>
2021-12-09 04:31:13 -0800

    feat: add global lua functions to utils module
    - A handy keymap function for structuring key bindings.
    - Output text from command to buffer attempt
    - Some useful commands like wrapping `inspect()` in a print statment

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,3 +1,51 @@
+-- Module: utils
+-- Description: Utility module
+
+
+--- Globals ---
+
+-- Esier Inspect lua objects
+function _G.inspect(args)
+  print(vim.inspect(args))
+end
+
+-- Useful functions to explore Lua objects
+-- https://www.reddit.com/comments/p84iu2
+
+function _G.put(...)
+  local objects = {}
+  for i = 1, select('#', ...) do
+    local v = select(i, ...)
+    table.insert(objects, vim.inspect(v))
+  end
+
+  print(table.concat(objects, '\n'))
+  return ...
+end
+
+function _G.put_text(...)
+  local objects = {}
+  for i = 1, select('#', ...) do
+    local v = select(i, ...)
+    table.insert(objects, vim.inspect(v))
+  end
+
+  local lines = vim.split(table.concat(objects, '\n'), '\n')
+  local lnum = vim.api.nvim_win_get_cursor(0)[1]
+  vim.fn.append(lnum, lines)
+  return ...
+end
+
+-- Easier mapping function
+function _G.map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+
+--- Module ---
+
 local M = {}
 
 -- Generate array from file lines
@@ -10,6 +58,7 @@ function M.fileToArray( fpath )
   return arr
 end
 
+-- Math operations
 -- Check if number is prime
 function M.is_prime(n)
   local max_divider_checked = math.sqrt(n) + 1

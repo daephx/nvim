407e41e5af00da11d6bd446105ba7f171274d462
daephx <36192863+daephx@users.noreply.github.com>
2021-12-09 02:19:04 -0800

    feat: definitions for nvimtree plugin
    Enables the plugin to be installed and loaded.
    NvimTree serves as a side pane file explorer for folder navigation.

---
+++ lua/plugins/nvimtree.lua
@@ -0,0 +1,117 @@
+-- nvim-tree | file browser for neovim
+
+local g = vim.g
+
+-- Prevent loading if not applicable
+local ok, nvimtree = pcall(require, 'nvim-tree')
+if not ok then
+  return
+end
+
+
+--- Globals ---
+
+g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened.
+g.nvim_tree_indent_markers = 1 -- This option shows indent markers when folders are open.
+g.nvim_tree_git_hl = 1 -- Will enable file highlight for git attributes (can be used without the icons).
+g.nvim_tree_highlight_opened_files = 1 -- Will enable folder and file icon highlight for opened files/directories.
+g.nvim_tree_add_trailing = 0 -- Append a trailing slash to folder names. ]]
+
+
+--- Setup ---
+
+nvimtree.setup {
+  -- disables netrw completely
+  disable_netrw       = true,
+  -- hijack netrw window on startup
+  hijack_netrw        = true,
+  -- open the tree when running this setup function
+  open_on_setup       = false,
+  -- will not open on setup if the filetype is in this list
+  ignore_ft_on_setup  = {},
+  -- closes neovim automatically when the tree is the last **WINDOW** in the view
+  auto_close          = false,
+  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+  open_on_tab         = false,
+  -- hijack the cursor in the tree to put it at the start of the filename
+  hijack_cursor       = true,
+  -- Auto update current directory
+  update_cwd          = true,
+  -- hijacks new directory buffers when they are opened.
+  update_to_buf_dir   = {
+    -- enable the feature
+    enable = true,
+    -- allow to open the tree if it was previously closed
+    auto_open = true,
+  },
+  -- show lsp diagnostics in the signcolumn
+  diagnostics = {
+    enable = true,
+    icons = {
+      hint = '',
+      info = '',
+      warning = '',
+      error = '',
+    }
+  },
+  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+  update_focused_file = {
+    -- enables the feature
+    enable      = true,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd  = true,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {}
+  },
+  -- configuration options for the system open command (`s` in the tree by default)
+  system_open = {
+    -- the command to run this, leaving nil should work in most cases
+    cmd  = nil,
+    -- the command arguments as a list
+    args = {}
+  },
+  filters = {
+    dotfiles = false,
+    custom = {'.git', 'node_modules', '.cache', '__pycache__'}
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 500,
+  },
+  view = {
+    -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
+    width = 40,
+    -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+    height = 30,
+    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+    side = 'right',
+    -- if true the tree will resize itself after opening a file
+    auto_resize = false,
+    mappings = {
+      -- custom only false will merge the list with the default mappings
+      -- if true, it will only use your list to set the mappings
+      custom_only = false,
+      -- list of mappings to set on the tree manually
+      list = {}
+    },
+    number = false,
+    relativenumber = false
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true
+  }
+}
+
+vim.cmd [[
+  highlight NvimTreeNormal guibg=none
+
+  function! DisableST()
+    return " "
+  endfunction
+  au! BufEnter NvimTree setlocal statusline=%!DisableST()
+  \ | setlocal cursorline
+]]

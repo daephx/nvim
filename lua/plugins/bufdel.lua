4fe1d7dba9e9a1b774fef7bd5cd19557c3cfa4b6
daephx <daephxdev@gmail.com>
2021-12-08 19:31:05 -0800

    refactor: remove settings for bufdel plugin
    I'm using a basic abbrev command for sane window management and no longer
    require this plugin.

--- lua/plugins/bufdel.lua
+++
@@ -1,8 +0,0 @@
-require('bufdel').setup {
-  next = 'cycle',  -- or 'alternate'
-  quit = true,
-}
-
--- Overwrite `:bd` command to call BufDel
--- This prevents windows resizing for things like Nerd/Nvim Tree
-vim.cmd[[:cnoreabbrev bd BufDel]]

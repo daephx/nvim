2db1d8f1e374cf72ed5f8780661bc5737bfb2048
daephx <daephxdev@gmail.com>
2021-12-14 19:13:01 -0800

    feat: add keybind for window switching
    Use the basic `<c-hjkl>` movements for switching the active window/split.
    As opposed to `<c-w>hjkl`

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,3 +9,9 @@ local api = vim.api
 -- Visually move lines <Up/Down>
 map('v', 'J', ":m '>+1<CR>gv=gv")
 map('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Ctrl - hjkl to navigate splits
+map('n', '<c-h>', '<cmd>wincmd h<CR>')
+map('n', '<c-j>', '<cmd>wincmd j<CR>')
+map('n', '<c-k>', '<cmd>wincmd k<CR>')
+map('n', '<c-l>', '<cmd>wincmd l<CR>')

11791d5185ea671cbaaad95d560258938d502e10
daephx <daephxdev@gmail.com>
2021-12-14 19:35:12 -0800

    feat: add whichkey mapping tables
    I prefer that as many custom keybindings are defined in the same file.
    To that end, these settings are specific to the lua reimplementation of
    whichkey. The script will check if it exists before loading the mappings
    so it is not a hard dependency. But I'd be lost without some of these
    settings.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -63,3 +63,113 @@ end
 -- Diffsplit
 -- map('n', 'q', "&diff ? '<cmd>tabclose<CR>' : 'q'")
 vim.cmd [[ nnoremap <expr> q &diff ? '<cmd>tabclose<CR>' : 'q' ]]
+
+
+--- Whichkey ---
+
+-- Check for whichkey plugin
+local ok, wk = pcall(require, "which-key")
+if not ok then
+  return
+end
+
+local whichkey_map = {}
+
+-- General
+whichkey_map = {
+  name = '+Whichkey',
+  ['<Space>'] = {'<cmd>Telescope buffers<CR>', 'Open Buffers'},
+  ['?'] = {'Cheatsheet'},
+  ['/'] = {'<Plug>kommentary_line_default', 'Comment Line'},
+  P = {'which_key_ignore'},
+  e = {'<cmd>NvimTreeToggle<cr>', 'Toggle NvimTree'},
+  q = {'LSP Set Location List'},
+}
+
+
+if pcall(require, "dap") then
+
+-- Debug Protocol
+whichkey_map.d = {
+  name = '+Debug',
+  b = {'<cmd>lua require("dap").toggle_breakpoint()<CR>', 'Set Breakpoint'},
+  d = {'<cmd>lua require("dap.python").attach_python_debugger()<CR>', 'Start Debugging'},
+  n = {'<cmd>lua require("dap").continue()<CR>', 'Continue debugging'},
+}
+
+end
+
+
+if pcall(require, "telescope") then
+
+-- Telescope Find
+whichkey_map.f = {
+  name = '+Files',
+  ['.'] = {'<cmd>lua require("plugins.telescope").search_dotfiles()<CR>', 'Dotfiles'},
+  ['<S-c>'] = {'<cmd>Telescope commands<CR>', 'Commands'},
+  ['<S-f>'] = {'<cmd>Telescope treesitter<CR>', 'Treesitter'},
+  b = {'<cmd>Telescope current_buffer_fuzzy_find<CR>', 'Grep Buffer'},
+  c = {'<cmd>Telescope colorscheme<CR>', 'ColorSchemes'},
+  d = {'<cmd>Telescope lsp_document_diagnostics<CR>', 'LSP Diagnostics'},
+  e = {'<cmd>Telescope file_browser<CR>', 'File Explorer'},
+  f = {'<cmd>Telescope find_files<CR>', 'Current Files'},
+  h = {'<cmd>Telescope highlights<CR>', 'Highlight Groups'},
+  l = {'<cmd>Telescope live_grep<CR>', 'Live Grep'},
+  m = {'<cmd>Telescope marks<CR>', 'Bookmarks'},
+  p = {'<cmd>Telescope projects<CR>', 'Project list'},
+  r = {'<cmd>Telescope oldfiles<CR>', 'Recent Files'},
+  s = {'<cmd>Telescope grep_string<CR>', 'Grep String'},
+  t = {'<cmd>TodoTelescope<CR>', 'Search Todo comments'},
+  v = {'<cmd>lua require("plugins.telescope").search_vimfiles()<CR>', 'VimRC'},
+  w = {'<cmd>lua require("plugins.telescope").search_wikifiles()<CR>', 'Wiki'},
+}
+
+
+
+end
+
+-- Plugins
+whichkey_map.p = {
+  name = '+Plugins',
+  c = {'<cmd>PackerCompile<CR>', 'Compile Plugins'},
+  s = {'<cmd>PackerStatus<CR>', 'Plugins status'},
+  u = {'<cmd>PackerCompile<CR><cmd>PackerSync<CR>', 'Update Plugins'},
+}
+
+-- Git
+whichkey_map.g = {
+  name = '+Git',
+  G = {'<cmd>lua require("neogit").open()<CR>', 'Git Status (Neogit)'},
+  b = {'<cmd>lua require("plugins.telescope").git_branches()<CR>', 'Git branches'},
+  d = {'<cmd>GdiffsplitTab<CR>', 'Diff current buffer'},
+  f = {'<cmd>Telescope git_files<CR>', 'Git files'},
+  g = {'<cmd>Gtabedit :<CR>', 'Git Status (Fugitive)'},
+  l = {'<cmd>GclogTab<CR>', 'Git Log'},
+  z = {'<cmd>lua require("plugins.toggleterm").lazygit:toggle()<CR>', 'Lazygit'},
+}
+
+-- New
+whichkey_map.n = {
+  name = '+New',
+  t = {'<cmd>tabnew<CR>', 'New tab'},
+  f = {'<cmd>enew<CR>', 'New file'},
+}
+
+-- Sessions
+whichkey_map.s = {
+  name = '+Session',
+  d = {'<cmd>DeleteSession<CR>', 'Delete current session'},
+  l = {'<cmd>Telescope session-lens search_session<CR>', 'Load last session'},
+  s = {'<cmd>SessionSave<CR>', 'Save current session'},
+}
+
+-- Terminal
+whichkey_map.t = {
+  name = '+Terminal',
+  t = {'<cmd>ToggleTerm direction=tab<CR>', 'Terminal Tab'},
+  d = {'<cmd>lua require("plugins.toggleterm").lazydocker:toggle()<CR>', 'Lazydocker'},
+  g = {'<cmd>lua require("plugins.toggleterm").lazygit:toggle()<CR>', 'Lazygit'},
+}
+
+-- Register Keymap
+wk.register(whichkey_map, { prefix = '<Leader>' })

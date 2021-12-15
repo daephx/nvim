990f5102cdf5ebacc544a4f4d2ba8aed8e2b78bb
daephx <36192863+daephx@users.noreply.github.com>
2021-12-14 19:24:30 -0800

    feat: add keybinds for controlling terminal buffers
    Some mappings require toggleterm plugin to be installed,
    Allows terminal buffers to be escaped with `<c-w>` to switch the focused
    split windows.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -25,3 +25,25 @@ map('n', '<c-q>', '<cmd>call ToggleQFList(1)<CR>')
 map('n', '<localleader>q', '<cmd>call ToggleQFList(1)<CR>')
 map('n', '<c-k>', '<cmd>cprev<CR>zz')
 map('n', '<c-j>', '<cmd>cnext<CR>zz')
+
+
+--- Terminal ---
+map('t', '<leader><esc>', '<c-\\><c-n>') -- Double escape terminal mode
+map('n', '<c-\\>', '<cmd>ToggleTerm direction=tab<CR>') -- Toggle tab terminal
+map('n', '<c-\\><c-\\>', '<cmd>ToggleTerm direction=horizontal<CR>') -- Toggle horizontal terminal
+map('t', '<c-\\>', '<c-\\><c-n><cmd>ToggleTerm<CR>') -- Escape and untoggle in terminal mode
+map('t', '<c-w>', '<c-\\><c-n><c-w>')  -- Allow Ctrl+W to switch window in terminal mode
+-- map('t', '<c-r>', '<c-r>')
+
+map('n', '<c->`', '<cmd>ToggleTerm direction=horizontal<CR>') -- Toggle horizontal terminal
+
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+end
+-- cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')

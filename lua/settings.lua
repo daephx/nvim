6ebd475eaecbee9e2d2e47e7550503265f2968b8
daephx <36192863+daephx@users.noreply.github.com>
2021-12-14 19:01:39 -0800

    style: apply indentation for has(win64) settings
    settings are only applied if the current device is nt/windows.

--- lua/settings.lua
+++ lua/settings.lua
@@ -148,10 +148,11 @@ opt.listchars = {
 -- These settings apply only to nt/windows
 
 if fn.has('win32') == 1 then
-  cmd([[ " Set default shell to powershell
-    let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-    set shellquote= shellpipe=\| shellxquote=
-    set shellcmdflag=-NoLogo\ -NoProfile\ -ExecutionPolicy\ RemoteSigned\ -Command
-    set shellredir=\|\ Out-File\ -Encoding\ UTF8
-  ]])
+  cmd [[
+  " Set default shell to powershell
+  let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+  set shellquote= shellpipe=\| shellxquote=
+  set shellcmdflag=-NoLogo\ -NoProfile\ -ExecutionPolicy\ RemoteSigned\ -Command
+  set shellredir=\|\ Out-File\ -Encoding\ UTF8
+  ]]
 end

51b7081fd608199392d59c7ed6d31c49c6dfabfa
daephx <36192863+daephx@users.noreply.github.com>
2021-12-16 09:17:25 -0800

    refactor: remove colors overrides for indent-blankline
    Removes lines for setting highlights for Indent-blanklines groups.

--- lua/colors/overrides.lua
+++ lua/colors/overrides.lua
@@ -23,16 +23,6 @@ function M.setup(table)
     utils.setHighlight('Comments', {ui = 'none'})
   end
 
-  if g.loaded_indent_blankline then
-    cmd [[ " Highlight all indentation lines
-    " highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine
-    " highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine
-    " highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine
-    " highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine
-    " highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine
-    " highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine
-    ]]
-  end
 
   vim.cmd([[
 

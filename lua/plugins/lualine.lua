7995f5b2b4f20d5217b232792c50d2ac47f4be84
daephx <36192863+daephx@users.noreply.github.com>
2021-12-16 05:09:00 -0800

    refactor: remove location from inactive status line
    Remove the cursor position indicator from inactive buffer status lines.
    This makes the file path the only available information and looks more
    minimal and less cluttered.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -91,7 +91,7 @@ lualine.setup {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },

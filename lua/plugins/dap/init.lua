09a17a48d2324f54eefbe06ffc276cb7c070fe22
daephx <daephxdev@gmail.com>
2021-12-24 20:04:49 -0800

    feat(dap): config loader for dap module
    The config function will now search `plugins/dap` for lua scripts that
    corresponding to the language they belong.
    
    If python exists in the language list, and `plugins/dap/python.lua`
    That module will be loaded to initialize the adapters and configurations.
    
    The language list above simply acts as a feature flag.
    to prevent language that are not in the list, i.e. untested:
    from being initialized.

--- lua/plugins/dap/init.lua
+++ lua/plugins/dap/init.lua
@@ -3,10 +3,32 @@
 vim.fn.sign_define('DapBreakpoint', { text = '●', texthl = 'DiagnosticError' })
 vim.fn.sign_define('DapStopped', { text = '◯', texthl = 'Number' })
 
+
+-- Languages defined here will be looked for in the plugin.dap module,
+-- Any lua script with the coorisponding name will be loaded if found.
+-- i.e. lua/plugins/dap/python.lua
+
+local languages = {
+  -- "cpp",
+  -- "cs",
+  -- "go",
+  -- "ruby",
+  -- "lua",
+  -- "python",
+  -- "rust",
+  -- "javascript",
+}
+
 local M = {}
 
 M.config = function()
-  -- require('dap.python')
+  -- initialize adapters/configs for languages
+  for _, lang in pairs(languages) do
+    if not pcall(require, 'plugins.dap.' .. lang) then
+      error("DAP: Config for " .. lang .. " is not defined!")
+      return
+    end
+  end
 end
 
 return M

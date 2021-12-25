831f72559469f6bc571583e28b923ba98b8b1b88
daephx <daephxdev@gmail.com>
2021-12-24 20:29:12 -0800

    feat(dap): custom remaps applied for debugging
    Define keybindings for debug controls:
    
    - Set breakpoints
    - Start/Continue Debugging
    - Step Into/Over/Out
    
    These mappings are only initialized if the dap plugin is loaded.
    Perhaps there would be a better way to have it only apply when the
    language is properly configured, similar to LSP.

--- lua/plugins/dap/init.lua
+++ lua/plugins/dap/init.lua
@@ -20,6 +20,24 @@ dap.listeners.after['event_terminated']['dapui'] = function()
   require('dapui').close()
 end
 
+local mappings = function()
+  map('n', '<leader>dC', '<cmd>lua require("dap").continue()<CR>')
+  map('n', '<leader>db', '<cmd>lua require("dap").toggle_breakpoint()<CR>')
+  map('n', '<leader>dB', '<cmd>lua require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))<cr>')
+  map('n', '<leader>do', '<cmd>lua require("dap").step_over()<CR>')
+  map('n', '<leader>dO', '<cmd>lua require("dap").step_out()<CR>')
+  map('n', '<leader>dn', '<cmd>lua require("dap").step_into()<CR>')
+  map('n', '<leader>dN', '<cmd>lua require("dap").step_back()<CR>')
+  map('n', '<leader>dr', '<cmd>lua require("dap").repl.toggle()<CR>')
+  map('n', '<leader>d.', '<cmd>lua require("dap").goto_()<CR>')
+  map('n', '<leader>dh', '<cmd>lua require("dap").run_to_cursor()<CR>')
+  map('n', '<leader>de', '<cmd>lua require("dap").set_exception_breakpoints()<CR>')
+  map('n', '<leader>dv', '<cmd>Telescope dap variables<CR>')
+  map('n', '<leader>dc', '<cmd>Telescope dap commands<CR>')
+  map('n', '<leader>dx', '<cmd>lua require("dapui").eval()<CR>')
+  map('x', '<leader>dx', '<cmd>lua require("dapui").eval()<CR>')
+  map('n', '<leader>dX', '<cmd>lua require("dapui").eval(vim.fn.input("expression: "))<CR>')
+end
 
 vim.cmd([[
 function! SetupREPL()
@@ -55,6 +73,7 @@ local languages = {
 local M = {}
 
 M.config = function()
+  mappings() -- initialize Debugger keymaps
   -- initialize adapters/configs for languages
   for _, lang in pairs(languages) do
     if not pcall(require, 'plugins.dap.' .. lang) then

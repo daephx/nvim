67abce80506952de155b605733e4dcd7a321404a
daephx <daephxdev@gmail.com>
2021-12-25 15:41:39 -0800

    style(cmp): fix indent for snippet expand configs

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -35,10 +35,10 @@ lspkind.init {
 cmp.setup {
   snippet = {
     expand = function(args)
-    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
     end,
 
   },

dfe25a3dfe5b796dae481505db228644c66819c0
daephx <daephxdev@gmail.com>
2022-01-06 04:55:07 -0800

    style: apply stylua on colors module after refactor

--- lua/colors/sakura.lua
+++ lua/colors/sakura.lua
@@ -1,14 +1,13 @@
 -- Sakura
 
-
 --- Highlights ---
 
 local M = {}
 
 M.highlights = {
-  String = {fg = '#cbaa73'},
-  CursorLineNr = {bg = 'NONE'},
-  SpecialChar = {fg = '#56b1bb'},
+  String = { fg = '#cbaa73' },
+  CursorLineNr = { bg = 'NONE' },
+  SpecialChar = { fg = '#56b1bb' },
 }
 
 return M

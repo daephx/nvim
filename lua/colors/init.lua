7878d6959145435140c2ca03d2492549f06d8336
daephx <daephxdev@gmail.com>
2022-01-07 23:53:43 -0800

    refactor: move color utils to init module
    - Remove utils module and requires reference
    - Rename utils setup function to `applyHighlight` which accepts tables
      as input for setting highlight groups.

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -14,9 +14,66 @@
 local g = vim.g
 
 local global = require('colors.global')
-local utils = require('colors.utils')
 
 local M = {}
+
+--- Utilities ---
+
+--@param  group  : string = 'DiffAdd'
+--@param  colors : table  = {'fg', 'gui'}
+--@return color  : string = '#10e010'
+function M.getHighlight(group, term)
+  return fn.synIDattr(fn.synIDtrans(fn.hlID(group)), unpack(term))
+end
+
+function M.getHighlightTable(group)
+  local output = fn.execute('hi ' .. group)
+  local list = fn.split(output, '\\s\\+')
+
+  local dict = {}
+  for _, item in pairs(list) do
+    if fn.match(item, '=') > 0 then
+      local splited = fn.split(item, '=')
+      dict[splited[1]] = splited[2]
+    end
+  end
+  return dict
+end
+
+-- TODO: Allow extraction of linked values and overwriting, aka partialLinks
+--@param group  : string = 'DiffAdd'
+--@param colors : table  = {fg = '#10e010'}
+function M.setHighlight(group, colors)
+  local bg = colors.bg and 'guibg=' .. colors.bg or ''
+  local fg = colors.fg and 'guifg=' .. colors.fg or ''
+  local sp = colors.sp and 'guisp=' .. colors.sp or ''
+  local ui = colors.ui and 'gui=' .. colors.ui or ''
+
+  local hl = 'silent highlight ' .. table.concat({ group, bg, fg, sp, ui }, ' ')
+
+  if colors.clear == true then
+    vim.cmd('highlight clear ' .. group)
+    return
+  end
+  if colors.link then
+    link = table.concat({ 'link', group, colors.link }, ' ')
+    vim.cmd('highlight! ' .. link)
+  end
+  vim.cmd(hl)
+end
+
+-- @param opts : { group = { fg = 'red', bg = 'blue' } }
+function M.applyHighlights(opts)
+  local next = next
+  opts = type(opts) == 'function' and opts() or opts
+  for group, colors in pairs(opts) do
+    if next(colors) ~= nil then
+      M.setHighlight(group, colors)
+    end
+  end
+end
+
+--- Initialization ---
 
 function M.setup(name)
   -- Check for colorscheme definition module
@@ -30,11 +87,11 @@ function M.setup(name)
 
   -- Load colorscheme specific overrides
   if ok and colorscheme.highlights then
-    utils.setup(colorscheme.highlights)
+    M.applyHighlights(colorscheme.highlights)
   end
 
   -- Apply global overrides
-  utils.setup(global.highlights)
+  M.applyHighlights(global.highlights)
 
   -- Update Colorscheme
   -- Reload this function when colorscheme is updated

0def37f8149f4c04d5b041438f882bd7fc63b632
daephx <daephxdev@gmail.com>
2022-01-08 02:51:03 -0800

    feat: add git conflict highlighter plugin
    Add install and config instructions for new plugin: [conflict-marker.vim](https://github.com/rhysd/conflict-marker.vim)
    
    The highlighting is very helpful with merge conflicts!

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -134,6 +134,27 @@ return packer.startup({
       disable = false,
     })
 
+    use({ -- Weapon to fight against conflicts in Vim.
+      'rhysd/conflict-marker.vim',
+      config = function()
+        vim.cmd([[
+        " disable the default highlight group
+        let g:conflict_marker_highlight_group = ''
+
+        " Include text after begin and end markers
+        let g:conflict_marker_begin = '^<<<<<<< .*$'
+        let g:conflict_marker_end   = '^>>>>>>> .*$'
+        let g:conflict_marker_separator = '^=======$'
+
+        highlight ConflictMarkerBegin guibg=#2f7366
+        highlight ConflictMarkerOurs guibg=#2e5049
+        highlight ConflictMarkerTheirs guibg=#344f69
+        highlight ConflictMarkerEnd guibg=#2f628e
+        highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
+        ]])
+      end,
+    })
+
     use({ -- Magit for Neovim
       'TimUntersberger/neogit',
       disable = false,

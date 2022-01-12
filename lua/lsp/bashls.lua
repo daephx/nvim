664a4b132e43927c1a219430d4419e0d9b6b2d8b
daephx <daephxdev@gmail.com>
2022-01-12 02:54:25 -0800

    refactor: upgrade bashls server setup module
    When installing from lsp_installer, it doesn't automatically handle the
    path in this environment, you have to make sure the root folder and
    binary are explicitly set.

--- lua/lsp/bashls.lua
+++ lua/lsp/bashls.lua
@@ -7,10 +7,23 @@
 -- For more information on setting up language-servers, See:
 -- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
 
+local bashls_bin
+local bashls_root = vim.fn.stdpath('data') .. '/lsp_servers/bash'
+if vim.fn.has('mac') == 1 then
+  bashls_bin = bashls_root .. '/node_modules/.bin/bash-language-server'
+elseif vim.fn.has('unix') == 1 then
+  bashls_bin = bashls_root .. '/node_modules/.bin/bash-language-server'
+elseif vim.fn.has('win32') == 1 then
+  bashls_bin = bashls_root .. '/node_modules/.bin/bash-language-server.exe'
+else
+  error('Unsupported system for bashls')
+end
+
 local M = {}
 
 M.setup = {
-  filetypes = { 'sh', 'zsh', 'bash' },
+  cmd = { bashls_bin, 'start' },
+  filetypes = { 'sh', 'bash', 'zsh' },
 }
 
 return M

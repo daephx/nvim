e161c0ad8cdb9739a447519bd8deca670ab0257d
daephx <daephxdev@gmail.com>
2022-02-21 02:41:02 -0800

    Merge branch 'fix-null-ls' onto develop
    - add rust formatting from null-ls builtins
    - add ansible playbook linting for null-ls
    - add clang formatting from null-ls builtins
    - add golang formatting from null-ls builtins
    
    commit 9e2a3b699877f9cb3b06fc82481b216f8bfa1e44
    Author: daephx <daephxdev@gmail.com>
    Date:   Thu Jan 20 21:53:05 2022 -0800
    
        feat: add gitsigns actions from null-ls builtins
    
        Allows for staging hunks using the gitsigns plugin from code actions menu.
    
    commit 225b856792fb4d638bb19442476cc9f5c02e7c0c
    Author: daephx <daephxdev@gmail.com>
    Date:   Sun Jan 16 20:47:44 2022 -0800
    
        feat: null-ls enable prettier with extra configs
    
        Apply specialized settings to prettier formatting via extra command
        arguments. Also include extra filetypes such as, toml and solidity.
    
    commit 8caeeb1118a7a1932592bd8eb6b49bf9f47d045a
    Author: daephx <daephxdev@gmail.com>
    Date:   Fri Jan 14 08:54:00 2022 -0800
    
        refactor: add local shorthand for null-ls builtins
    
        - Define imports before module table.
        - Add local variables to reduce typing;
          Then change all references.

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -1,48 +1,61 @@
 -- Null-ls | Neovim language server
 -- url: https://github.com/jose-elias-alvarez/null-ls.nvim
 
-local M = {}
-
 local null_ls = require('null-ls')
+
+local code_actions = null_ls.builtins.code_actions
+local completion = null_ls.builtins.completion
+local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+
+local M = {}
 
 M.setup = function()
   null_ls.setup({
     debug = false,
     sources = {
-      null_ls.builtins.completion.spell,
-      null_ls.builtins.completion.luasnip,
+      completion.spell,
+      completion.luasnip,
 
-      -- null_ls.builtins.formatting.lua_format,
-      null_ls.builtins.formatting.stylua.with({
+      -- Git - Version Control
+      diagnostics.gitlint,
+      code_actions.gitsigns,
+
+      -- formatting.lua_format,
+      formatting.stylua.with({
         condition = function(utils)
           return utils.root_has_file({ 'stylua.toml', '.stylua.toml' })
         end,
       }),
 
       -- Shell scripts
-      null_ls.builtins.code_actions.shellcheck,
-      null_ls.builtins.diagnostics.shellcheck.with({
+      -- formatting.shfmt,
+      code_actions.shellcheck,
+      diagnostics.shellcheck.with({
         -- diagnostics_format = "[#{c}] #{m} (#{s})"
       }),
-      -- null_ls.builtins.formatting.shfmt,
 
       -- Python
-      null_ls.builtins.diagnostics.flake8,
-      null_ls.builtins.formatting.black,
-      null_ls.builtins.formatting.isort,
+      diagnostics.flake8,
+      formatting.black,
+      formatting.isort,
 
-      -- null_ls.builtins.diagnostics.eslint,
-      -- null_ls.builtins.formatting.prettier,
-      -- null_ls.builtins.formatting.clang_format,
-      -- null_ls.builtins.formatting.gofmt,
-      -- null_ls.builtins.formatting.goimports,
-      -- null_ls.builtins.formatting.rustfmt,
+      -- Golang
+      formatting.gofmt,
+      formatting.goimports,
+
+      -- C/C++/Rust
+      formatting.clang_format,
+      formatting.rustfmt,
+
+      -- diagnostics.eslint,
+      formatting.prettier.with({
+        extra_filetypes = { 'toml', 'solidity' },
+        extra_args = { '--no-semi', '--single-quote', '--jsx-single-quote' },
+      }),
 
       -- Linter for Ansible playbooks, roles and collections.
-      -- null_ls.builtins.diagnostics.ansiblelint
-
-      -- Linter for git commit messages
-      -- null_ls.builtins.diagnostics.gitlint,
+      diagnostics.ansiblelint,
     },
   })
 end

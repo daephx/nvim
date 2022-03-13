e2f6f9a2d9995c0cc3da474b60e6d353e12cfe96
daephx <daephxdev@gmail.com>
2022-03-12 19:01:52 -0800

    style: move tabline setup comments to newline

--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -8,11 +8,16 @@ tabline.setup({
     -- These options can be used to override those settings.
     component_separators = { '', '' },
     section_separators = { '', '' },
-    max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
-    show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
-    show_devicons = true, -- this shows devicons in buffer section
-    show_bufnr = false, -- this appends [bufnr] to buffer section,
-    show_filename_only = true, -- shows base filename only instead of relative path in filename
+    -- set to nil by default, and it uses vim.o.columns * 2/3
+    max_bufferline_percent = 66,
+    -- this shows tabs only when there are more than one tab or if the first tab is named
+    show_tabs_always = false,
+    -- this shows devicons in buffer section
+    show_devicons = true,
+    -- this appends [bufnr] to buffer section,
+    show_bufnr = false,
+    -- shows base filename only instead of relative path in filename
+    show_filename_only = true,
   },
 })
 

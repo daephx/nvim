54aedea83bbd50d88d761be9106c2ffdeba6c5c1
daephx <daephxdev@gmail.com>
2022-03-12 18:24:26 -0800

    refactor: relative filepath on inactive statusline
    Display a shorter path in non-active status line.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -91,7 +91,7 @@ lualine.setup({
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { 'filename' },
+    lualine_c = { { 'filename', path = 1 } },
     lualine_x = {},
     lualine_y = {},
     lualine_z = {},

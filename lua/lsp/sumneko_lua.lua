4332124c8bd0806809b65dc0dadf2bf333d3848c
daephx <daephxdev@gmail.com>
2022-03-18 17:11:24 -0700

    fix: sumneko lsp long loads and environment prompt
    Fixes: #5
    
    Sumneko supports lua LOVE as a third party environment, but currently is
    more of a nuisance when opening any sort of lua file for neovim config.
    This disables the 3rd party check, eliminating the annoying prompt.
    
    On top of that, initialization time for the sumneko language server was
    taking a day and a half to fully load. This seems to be caused by making
    the lsp aware of all runtime files which includes packer plugin files.
    
    This could be helpful for developing and debugging plugins but basically
    renders the language server useless for up to 10 minutes on startup.

--- lua/lsp/sumneko_lua.lua
+++ lua/lsp/sumneko_lua.lua
@@ -45,9 +45,10 @@ M.setup = {
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true),
+        -- library = vim.api.nvim_get_runtime_file('', true),
         maxPreload = 2000,
         preloadFileSize = 50000,
+        checkThirdParty = false,
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {

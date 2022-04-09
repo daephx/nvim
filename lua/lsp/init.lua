63e5700f255ee279f538f9ee410f83dfebd7a55c
daephx <daephxdev@gmail.com>
2022-04-09 06:56:04 -0700

    refactor: move lsp keymaps to local function
    Call `lsp_keymaps` from `on_attach` to separate functionality.

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -42,24 +42,14 @@ end
 -- Close LspInfo window using 'q'
 vim.cmd([[ autocmd! FileType lspinfo nnoremap <silent> <buffer> qq :q<CR> ]])
 
--- Overriding the default LSP server options
+--- Mappings ---
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-M.on_attach = function(client, bufnr)
+local function lsp_keymaps(bufnr)
   local function buf_set_keymap(...)
-    api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local function buf_set_option(...)
-    api.nvim_buf_set_option(bufnr, ...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
   end
 
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
   local opts = { noremap = true, silent = true }
-
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
@@ -90,6 +80,20 @@ M.on_attach = function(client, bufnr)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
   buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+-- Use an on_attach function to set LSP related actions for
+-- when the language server attaches to the current buffer
+M.on_attach = function(client, bufnr)
+  local function buf_set_option(...)
+    api.nvim_buf_set_option(bufnr, ...)
+  end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Enable LSP Mappings
+  lsp_keymaps(bufnr)
 
   -- Show line diagnostics on cursor position in hover window
   vim.cmd(

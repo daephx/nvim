1d3f84aa60fb530d281ba91eb32e19b8049b98ca
daephx <daephxdev@gmail.com>
2022-04-16 17:51:48 -0700

    feat(indent-blankline): enable first indentation level
    - Display indent line for first indentation level

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -18,7 +18,7 @@ indent_blankline.setup({
   enabled = true,
   char_highlight = 'LineNr',
   show_end_of_line = false,
-  show_first_indent_level = false,
+  show_first_indent_level = true,
   space_char_blankline = ' ',
   show_trailing_blankline_indent = false,
   show_current_context = true,

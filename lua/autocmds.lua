8589fd736813baf9af0e2c2a5edbb8b500d62f34
daephx <daephxdev@gmail.com>
2022-06-01 20:09:42 -0700

    refactor: new auto relative number autocmd
    Replace messy vimscript plugin with basic autocmd: refactored to lua

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -6,3 +6,16 @@ api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
   group = 'DisableSearchHighlight',
   command = 'nohlsearch',
 })
+
+api.nvim_create_augroup('SmartHybridNumbers', { clear = true })
+api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'WinEnter' }, {
+  desc = 'Intelligent Relative Numbers | Enable',
+  group = 'SmartHybridNumbers',
+  command = 'if &nu && mode() != "i" | set rnu | endif',
+})
+
+api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'WinLeave' }, {
+  desc = 'Intelligent Relative Numbers | Disable',
+  group = 'SmartHybridNumbers',
+  command = 'if &nu | set nornu | endif',
+})

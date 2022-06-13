decb2b827ad963b9b5c65da3ed3152a1ee3d109e
daephx <daephxdev@gmail.com>
2022-06-13 01:36:02 -0700

    fix: fugitive `after/ftplugin` autocmd / keymap
    Enable keymap opts `{ remap = true }` for fugitive filetypes

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -11,17 +11,6 @@ vim.opt_local.relativenumber = false
 
 --- Keymaps ---
 
---- Autocmds ---
-
-vim.api.nvim_create_augroup('FugitiveBuffer', {})
-vim.api.nvim_create_autocmd('User', {
-  desc = 'Apply setting to FugitiveIndex buffer',
-  pattern = 'FugitiveIndex',
-  group = 'FugitiveBuffer',
-  callback = function()
-    local opts = { buffer = 0, remap = false, silent = true }
-    -- Fugitive status index
-    vim.keymap.set('n', '<tab>', '=', opts)
-    vim.keymap.set('n', '<q>', 'gq', opts)
-  end,
-})
+local opts = { buffer = true, remap = true, silent = true }
+vim.keymap.set('n', '<tab>', '=', opts)
+vim.keymap.set('n', 'q', 'gq', opts)

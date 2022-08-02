52825b2eaf2903ad264e0c80f6071a17fa9091e8
Daemon <daephxdev@gmail.com>
2022-08-02 00:46:40 -0700

    fix: update nvimtree config (#14)
    * refactor: rename nvimtree config file to 'nvim-tree'
    
    * refactor: rename pcall status variable
    
    Make it more generic, doesn't really matter.
    
    * refactor: rename nvimtree augroup
    
    * style: remove comment strings from nvimtree.setup
    
    Comment strings were pasted from example config on repository documentation.
    These are helpful but not required, give that all values can be referenced by
    the builtin help documentation.
    
    `:h nvimtree`
    
    * style: move nvimtree actions higher in config table
    
    Moved higher in setup table for loose alphabetical order.
    
    * style: move nvimtree view numbers higher in config
    
    * fix: remove nvimtree `view.auto_resize`
    
    Setting should be relocated for new config format.
    `nvim-tree.actions.open_file.resize_window`
    
    * fix: rename nvimtree update_cwd
    
    Setting should be renamed for new config format.
    `nvim-tree.update_focused_file.update_root`
    
    * refactor: nvimtree enabled group empty folders
    
    Compact folders that only contain a single folder into one node in the file tree.
    
    - Similar to Vscode and Github by displaying folder paths.
    
    * refactor: remove `clear` from augroup(NvimTreeBuffer)
    
    Option defaults to `{clear = true}`, no need to explicitly set `true`.
    
    * fix: rename nvimtree `update_cwd`
    
    Rename setup variable for updated config format.
    `nvim-tree.sync_root_with_cwd`
    
    * style: reorder nvimtree top level setup vars
    
    * fix: rename nvimtree `update_to_buf_dir`
    
    Rename setup variable for updated config format.
    `nvim-tree.hijack_directories`
    
    * refactor: nvimtree enable case sensitive sorting

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,112 @@
+-- nvim-tree | file browser for neovim
+-- URL: https://github.com/kyazdani42/nvim-tree.lua
+local status_ok, nvimtree = pcall(require, 'nvim-tree')
+if not status_ok then
+  return
+end
+
+--- Setup ---
+
+nvimtree.setup({
+  sort_by = 'case_sensitive',
+  open_on_setup = false,
+  open_on_tab = false,
+  sync_root_with_cwd = true,
+  disable_netrw = true,
+  hijack_netrw = true,
+  hijack_cursor = true,
+  ignore_ft_on_setup = {},
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
+  actions = {
+    change_dir = {
+      enable = true,
+      global = false,
+    },
+    open_file = {
+      quit_on_open = false,
+      resize_window = false,
+    },
+  },
+  diagnostics = {
+    enable = true,
+    icons = {
+      hint = ' ',
+      info = ' ',
+      warning = ' ',
+      error = ' ',
+    },
+  },
+  update_focused_file = {
+    enable = true,
+    update_root = true,
+    ignore_list = {},
+  },
+  system_open = {
+    cmd = nil,
+    args = {},
+  },
+  filters = {
+    dotfiles = false,
+    custom = {
+      '.cache',
+      '.git',
+      '__pycache__',
+      'node_modules',
+    },
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 500,
+  },
+  view = {
+    number = false,
+    relativenumber = false,
+    width = 40,
+    height = 30,
+    side = 'right',
+    mappings = {
+      custom_only = false,
+      list = {},
+    },
+  },
+  renderer = {
+    add_trailing = false,
+    highlight_git = true,
+    highlight_opened_files = '2',
+    group_empty = true,
+    indent_markers = {
+      enable = true,
+      icons = {
+        corner = '└ ',
+        edge = '│ ',
+        none = '  ',
+      },
+    },
+  },
+  trash = {
+    cmd = 'trash',
+    require_confirm = true,
+  },
+})
+
+vim.api.nvim_create_augroup('NvimTreeBuffer', {})
+vim.api.nvim_create_autocmd('FileType', {
+  desc = 'Apply local settings to NvimTree buffer',
+  group = 'NvimTreeBuffer',
+  pattern = 'NvimTree',
+  callback = function()
+    -- Highlights
+    vim.api.nvim_set_hl(0, 'NvimTreeNormal', { link = 'Normal' })
+    -- Settings
+    vim.opt_local.cursorlineopt = 'both'
+    vim.opt_local.cursorline = true
+    vim.opt_local.statusline = ' '
+    -- Keymaps
+    local opts = { buffer = 0, silent = true, noremap = true }
+    vim.keymap.set('n', '<esc>', '<cmd>q<cr>', opts)
+  end,
+})

258ebcc31f0f2765e960615d8295cdaaff636a97
daephx <daephxdev@gmail.com>
2022-09-12 14:44:31 -0700

    refactor(gitsigns): cleanup configuration file
    - Add metadata documentation comment for plugin
    - Add pcall guard to prevent runtime errors
    - Remove unused settings that match plugin default

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,9 @@
-local gitsigns = require('gitsigns')
+-- gitsigns.nvim | Git integration for buffers
+-- https://github.com/lewis6991/gitsigns.nvim
+local gitsigns_ok, gitsigns = pcall(require, 'gitsigns')
+if not gitsigns_ok then
+  return
+end
 
 gitsigns.setup({
   signs = {
@@ -49,7 +54,6 @@ gitsigns.setup({
       expr = true,
       "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'",
     },
-
     ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
     ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
     ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
@@ -66,32 +70,7 @@ gitsigns.setup({
     ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
   },
   watch_gitdir = {
-    interval = 1000,
     follow_files = true,
   },
-  attach_to_untracked = true,
-  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-  current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
-  },
-  current_line_blame_formatter_opts = {
-    relative_time = false,
-  },
   sign_priority = 6,
-  update_debounce = 100,
-  status_formatter = nil, -- Use default
-  max_file_length = 40000,
-  preview_config = {
-    -- Options passed to nvim_open_win
-    border = 'single',
-    style = 'minimal',
-    relative = 'cursor',
-    row = 0,
-    col = 1,
-  },
-  yadm = {
-    enable = false,
-  },
 })

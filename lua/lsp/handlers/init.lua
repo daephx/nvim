5211e41ea1872e6070a27aace53a190eaac7ab48
daephx <daephxdev@gmail.com>
2022-09-29 02:15:14 -0700

    refactor(lsp): move document highlight to handlers module

--- lua/lsp/handlers/init.lua
+++ lua/lsp/handlers/init.lua
@@ -4,9 +4,9 @@ local M = {}
 
 local codelens = require('lsp.handlers.codelens')
 local formatting = require('lsp.handlers.formatting')
+local highlight = require('lsp.handlers.highlight')
 local hover = require('lsp.handlers.hover')
 local keymaps = require('lsp.keymaps')
-local utils = require('lsp.utils')
 local signature = require('lsp.handlers.signature')
 
 -- Use an on_attach function to set LSP related actions for
@@ -14,12 +14,11 @@ local signature = require('lsp.handlers.signature')
 M.default_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Enable Local handlers
   codelens.enable_code_lens(client, bufnr)
   formatting.enable_auto_formatting(client, bufnr)
-  utils.enable_document_highlighting(client, bufnr)
+  highlight.enable_document_highlighting(client, bufnr)
   hover.enable_hover_diagnostics(bufnr)
-
-  -- Enable LSP Mappings
   keymaps.initialize_keymaps(client, bufnr)
   -- signature.enable_signature_help(client, bufnr)
 end
--- lua/lsp/utils.lua
+++ lua/lsp/utils.lua
@@ -4,35 +4,6 @@ local M = {}
 
 local lspconfig = require('lspconfig')
 
--- Highlight references under cursor
-M.enable_document_highlighting = function(client, bufnr)
-  if client.supports_method('textDocument/documentHighlight') then
-    local augroup = vim.api.nvim_create_augroup('LspDocumentHighlight', { clear = true })
-    vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
-      desc = 'Link LSP document reference highlighting to IncSearch',
-      group = augroup,
-      callback = function()
-        local opts = { link = 'IncSearch', bold = true }
-        vim.api.nvim_set_hl(0, 'LspReferenceRead', opts)
-        vim.api.nvim_set_hl(0, 'LspReferenceText', opts)
-        vim.api.nvim_set_hl(0, 'LspReferenceWrite', opts)
-      end,
-    })
-    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-      buffer = bufnr,
-      desc = 'Apply LSP documment reference highlighting on CursorHold',
-      group = augroup,
-      callback = vim.lsp.buf.document_highlight,
-    })
-    vim.api.nvim_create_autocmd({ 'CursorMoved', 'InsertEnter', 'CursorMovedI' }, {
-      buffer = bufnr,
-      desc = 'Disable LSP documment reference highlighting on CursorMoved',
-      group = augroup,
-      callback = vim.lsp.buf.clear_references,
-    })
-  end
-end
-
 local get_installed_servers = function()
   local lsp_installer = require('nvim-lsp-installer')
   -- Get configured servers from lsp-installer

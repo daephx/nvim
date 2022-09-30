d7496bc5832d500401164a83047cc8788c3d2a69
daephx <daephxdev@gmail.com>
2022-09-29 17:25:09 -0700

    refactor(lsp): improve buf keymap helper to accept opts

--- lua/lsp/keymaps.lua
+++ lua/lsp/keymaps.lua
@@ -1,43 +1,42 @@
 local M = {}
 
---- Mappings ---
-
-M.initialize_keymaps = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
+M.initialize_keymaps = function(_, bufnr)
+  local defaults = { buffer = bufnr, remap = false, silent = true }
+  local function map(mode, k, cb, opts)
+    local options = vim.tbl_extend('force', defaults, opts)
+    vim.keymap.set(mode, k, cb, options)
   end
 
-  local opts = { noremap = true, silent = true }
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap(
+  map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
+  map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
+  map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {})
+  map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {})
+  map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {})
+  map('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', {})
+  map('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', {})
+  map(
     'n',
     '<leader>wl',
     '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-    opts
+    {}
   )
-  buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap(
+  map('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', {})
+  map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', {})
+  map(
     'n',
     '<leader>ca',
     '<cmd>lua require("telescope.builtin").lsp_code_actions(require("telescope.themes").get_cursor())<CR>',
-    opts
+    {}
   )
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-  buf_set_keymap('v', 'gf', "<cmd>'<,'>lua vim.lsp.buf.range_formatting()<CR>", opts)
-  buf_set_keymap('n', '<leader>de', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {})
+  map('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>', {})
+  map('v', 'gf', "<cmd>'<,'>lua vim.lsp.buf.range_formatting()<CR>", {})
+  map('n', '<leader>de', '<cmd>lua vim.diagnostic.open_float()<CR>', {})
+  map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', {})
+  map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', {})
+  map('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', {})
+  map('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', {})
 end
 
 return M

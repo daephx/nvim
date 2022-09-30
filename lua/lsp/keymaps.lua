9712fd9485df5868f1c1fddeb06650b55ac79e07
daephx <daephxdev@gmail.com>
2022-09-29 17:31:06 -0700

    refactor(lsp): set keymaps to use lua callbacks
    - Prefer callbacks where applicable over `vim.cmd` strings
    - Remove keymap for telescope code action builtin | no longer available

--- lua/lsp/keymaps.lua
+++ lua/lsp/keymaps.lua
@@ -8,35 +8,27 @@ M.initialize_keymaps = function(_, bufnr)
   end
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {})
-  map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
-  map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {})
-  map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {})
-  map('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {})
-  map('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', {})
-  map('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', {})
-  map(
-    'n',
-    '<leader>wl',
-    '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-    {}
-  )
-  map('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', {})
-  map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', {})
-  map(
-    'n',
-    '<leader>ca',
-    '<cmd>lua require("telescope.builtin").lsp_code_actions(require("telescope.themes").get_cursor())<CR>',
-    {}
-  )
-  map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', {})
-  map('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>', {})
+  map('n', 'gD', vim.lsp.buf.declaration, {})
+  map('n', 'gd', vim.lsp.buf.definition, {})
+  map('n', 'K', vim.lsp.buf.hover, {})
+  map('n', 'gi', vim.lsp.buf.implementation, {})
+  map('n', '<C-k>', vim.lsp.buf.signature_help, {})
+  map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, {})
+  map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, {})
+  map('n', '<leader>wl', function()
+    local folders = vim.lsp.buf.list_workspace_folders()
+    print(vim.inspect(folders))
+  end, {})
+  map('n', '<leader>D', vim.lsp.buf.type_definition, {})
+  map('n', '<leader>rn', vim.lsp.buf.rename, {})
+  map('n', 'gr', vim.lsp.buf.references, {})
+  map('n', 'gf', vim.lsp.buf.formatting, {})
   map('v', 'gf', "<cmd>'<,'>lua vim.lsp.buf.range_formatting()<CR>", {})
-  map('n', '<leader>de', '<cmd>lua vim.diagnostic.open_float()<CR>', {})
-  map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', {})
-  map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', {})
-  map('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', {})
-  map('n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', {})
+  map('n', '<leader>de', vim.diagnostic.open_float, {})
+  map('n', '[d', vim.diagnostic.goto_prev, {})
+  map('n', ']d', vim.diagnostic.goto_next, {})
+  map('n', '<leader>q', vim.diagnostic.setloclist, {})
+  map('n', '<leader>f', vim.lsp.buf.formatting, {})
 end
 
 return M

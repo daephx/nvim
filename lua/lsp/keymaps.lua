9551c1fc394383c5c648d4c280c277eacfda577f
daephx <daephxdev@gmail.com>
2022-09-29 17:35:19 -0700

    refactor(lsp): add desc to `lsp.buf` keymaps
    - Add description to lsp buffer keymaps.
      Useful for whichkey, which displays the `desc` string.
    
    - Remove duplicate document formatting keymap.

--- lua/lsp/keymaps.lua
+++ lua/lsp/keymaps.lua
@@ -8,27 +8,58 @@ M.initialize_keymaps = function(_, bufnr)
   end
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  map('n', 'gD', vim.lsp.buf.declaration, {})
-  map('n', 'gd', vim.lsp.buf.definition, {})
-  map('n', 'K', vim.lsp.buf.hover, {})
-  map('n', 'gi', vim.lsp.buf.implementation, {})
-  map('n', '<C-k>', vim.lsp.buf.signature_help, {})
-  map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, {})
-  map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, {})
+  map('n', 'gD', vim.lsp.buf.declaration, {
+    desc = 'Goto declaration',
+  })
+  map('n', 'gd', vim.lsp.buf.definition, {
+    desc = 'Goto definition',
+  })
+  map('n', 'K', vim.lsp.buf.hover, {
+    desc = 'Show hover diagnostic',
+  })
+  map('n', 'gi', vim.lsp.buf.implementation, {
+    desc = 'Goto implementation',
+  })
+  map('n', '<C-k>', vim.lsp.buf.signature_help, {
+    desc = 'Signature Help',
+  })
+  map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, {
+    desc = 'Add workspace folder',
+  })
+  map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, {
+    desc = 'Remove workspace folder',
+  })
   map('n', '<leader>wl', function()
     local folders = vim.lsp.buf.list_workspace_folders()
     print(vim.inspect(folders))
-  end, {})
-  map('n', '<leader>D', vim.lsp.buf.type_definition, {})
-  map('n', '<leader>rn', vim.lsp.buf.rename, {})
-  map('n', 'gr', vim.lsp.buf.references, {})
-  map('n', 'gf', vim.lsp.buf.formatting, {})
-  map('v', 'gf', "<cmd>'<,'>lua vim.lsp.buf.range_formatting()<CR>", {})
-  map('n', '<leader>de', vim.diagnostic.open_float, {})
-  map('n', '[d', vim.diagnostic.goto_prev, {})
-  map('n', ']d', vim.diagnostic.goto_next, {})
-  map('n', '<leader>q', vim.diagnostic.setloclist, {})
-  map('n', '<leader>f', vim.lsp.buf.formatting, {})
+  end, { desc = 'Show workspace folders' })
+  map('n', '<leader>D', vim.lsp.buf.type_definition, {
+    desc = 'Show type definitions',
+  })
+  map('n', '<leader>rn', vim.lsp.buf.rename, {
+    desc = 'Rename reference',
+  })
+  map('n', 'gr', vim.lsp.buf.references, {
+    desc = 'Goto references',
+  })
+  map('n', 'gf', vim.lsp.buf.formatting, {
+    desc = 'Format document',
+  })
+  map('v', 'gf', "<cmd>'<,'>lua vim.lsp.buf.range_formatting()<CR>", {
+    desc = 'Format document',
+  })
+  map('n', '<leader>de', vim.diagnostic.open_float, {
+    desc = 'Open diagnostic float',
+  })
+  map('n', '[d', vim.diagnostic.goto_prev, {
+    desc = 'Previous diagnostic',
+  })
+  map('n', ']d', vim.diagnostic.goto_next, {
+    desc = 'Next diagnostic',
+  })
+  map('n', '<leader>q', vim.diagnostic.setloclist, {
+    desc = 'Diagnostic loclist',
+  })
 end
 
 return M

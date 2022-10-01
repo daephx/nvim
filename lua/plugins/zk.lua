d3b43d9cf771f4a1c742631c71e88bc1b08241f4
Daemon <daephxdev@gmail.com>
2022-10-01 04:45:41 -0700

    feat!: update markdown management plugins (#24)
    - Remove plugin: vimwiki
    
    - Add plugin: zk-nvim
    - Remove lsp: Zk
    
    Zk lsp configuration has been removed in favor of a dedicated plugin:
    
    - https://github.com/mickael-menu/zk-nvim
    
    This plugin handles settings up the language server internally,
    and provides helpful commands that integrate with telescope.

---
+++ lua/plugins/zk.lua
@@ -0,0 +1,25 @@
+-- zk-nvim | Neovim extension for zk
+-- https://github.com/mickael-menu/zk-nvim
+local zk_ok, zk = pcall(require, 'zk')
+if not zk_ok then
+  return
+end
+
+zk.setup({
+  -- can be "telescope", "fzf" or "select" (`vim.ui.select`)
+  -- it's recommended to use "telescope" or "fzf"
+  picker = 'telescope',
+})
+
+-- Keymap helper function
+local defaults = { remap = false, silent = true }
+local function map(mode, k, cb, opts)
+  local options = vim.tbl_extend('force', defaults, opts)
+  map(mode, k, cb, options)
+end
+
+-- Define zk keymaps
+map('n', '<leader>zn', "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", { desc = 'new note' })
+map('n', '<leader>Zb', '<Cmd>ZkBacklinks<CR>', { desc = 'show backlinks' })
+map('n', '<leader>Zl', '<Cmd>ZkLinks<CR>', { desc = 'show links' })
+map('n', '<leader>Zf', '<Cmd>ZkNotes<CR>', { desc = 'show notes' })

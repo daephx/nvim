68981f80979c8acb903334536da85976a03d7a3a
daephx <daephxdev@gmail.com>
2022-10-04 03:55:39 -0700

    docs(nvim): update comment lines for options module

--- lua/options.lua
+++ lua/options.lua
@@ -1,74 +1,70 @@
---    _____      __  __  _
---   / ___/___  / /_/ /_(_)___  ____ ______
---   \__ \/ _ \/ __/ __/ / __ \/ __ `/ ___/
---  ___/ /  __/ /_/ /_/ / / / / /_/ (__  )
--- /____/\___/\__/\__/_/_/ /_/\__, /____/
--- ==========================/____/========
--- Module: settings
--- Description: settings / options for Neovim
-
---- Settings ---
-
--- General settings for Vim/Neovim
+--    ____        __  _
+--   / __ \____  / /_(_)___  ____  _____
+--  / / / / __ \/ __/ / __ \/ __ \/ ___/
+-- / /_/ / /_/ / /_/ / /_/ / / / (__  )
+-- \____/ .___/\__/_/\____/_/ /_/____/
+-- ====/_/==============================
+-- Neovim general editor options/settings
 
 -- Set reusable values
-local encode = 'utf-8' -- Default File encoding
-local indent = 2 -- Default indentation
-local linelimit = 102 -- Default Limit for line length
+local encode = 'utf-8' -- File encoding
+local indent = 2 -- Indentation size
+local linelimit = 102 -- Limit for line length
 
--- Apply Settings
+-- General options
 vim.opt.autoread = true -- Load changes made outside of Vim
-vim.opt.autochdir = false -- Disable auto cd to open file
+vim.opt.autochdir = false -- Change working directory to active buffer
 vim.opt.smartindent = false -- Insert indents automatically
 vim.opt.autoindent = false -- Auto apply indentation
-vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.expandtab = true -- Expand tabs characters to spaces
 vim.opt.shiftround = true -- Round indent to shift width
 vim.opt.tabstop = indent -- Number of spaces for tab count
-vim.opt.softtabstop = indent
-vim.opt.shiftwidth = indent -- Size of an indent
+vim.opt.softtabstop = indent -- Spaces a <Tab> counts during edit operations
+vim.opt.shiftwidth = indent -- Number of spaces for indents
 vim.opt.encoding = encode -- Explicitly set file encoding
-vim.opt.fileencoding = encode
-vim.opt.textwidth = linelimit -- Limit length of characters in a line
+vim.opt.fileencoding = encode -- Set File-content encoding for the current buffer
+vim.opt.textwidth = linelimit -- Limit length of characters per line
 vim.opt.linebreak = true -- Split on line break
 vim.opt.colorcolumn = '-1' -- Visually display character line limit
-vim.opt.background = 'dark'
-vim.opt.clipboard = 'unnamed,unnamedplus' -- Use system clipboard
+vim.opt.background = 'dark' -- Adjusts the default color groups for background type
+vim.opt.clipboard = 'unnamed,unnamedplus' -- Configure system clipboard support
 vim.opt.cmdheight = 1 -- Command line character height
-vim.opt.conceallevel = 0
+vim.opt.conceallevel = 0 -- Determine how text with the "conceal" syntax is handled
 vim.opt.foldmethod = 'indent' -- Fold by method (indent & syntax are both good
 vim.opt.foldlevelstart = 9 -- We want all fold to be expand at start
 vim.opt.foldnestmax = 9 -- Max fold nest levels
-vim.opt.cindent = true
+vim.opt.cindent = false -- Indent using C indenting rules
 vim.opt.wrap = false -- Wrap text when line is too long
 vim.opt.hidden = true -- Hide modified buffers instead of unloading them
 vim.opt.cursorline = true -- Highlight the text line of the cursor
-vim.opt.cursorlineopt = 'number' -- settings for how 'cursorline' is displayed.
+vim.opt.cursorlineopt = 'number' -- Control how 'cursorline' is displayed
 vim.opt.incsearch = true -- Enable incremental search
-vim.opt.hlsearch = true -- Highlight search
+vim.opt.hlsearch = true -- Enable search highlighting
 vim.opt.spell = true -- Control vim Spell checking
 vim.opt.laststatus = 3 -- Apply statusline options
 vim.opt.lazyredraw = true -- Execute macros faster
 vim.opt.mouse = 'a' -- Enable mouse support
-vim.opt.showmode = false -- No EOL message
+vim.opt.showmode = false -- Manage whether EOL message is shown
 vim.opt.pumblend = 24 -- Pop-up menu transparency
 vim.opt.pumheight = 12 -- Pop-up menu height
 vim.opt.pumwidth = 16 -- Pop-up menu thickness
-vim.opt.ruler = true -- Always Show cursor position
-vim.opt.scrolloff = 8 -- Keep cursor closer to center
+vim.opt.ruler = true -- Always show cursor position
+vim.opt.scrolloff = 8 -- Keep cursor closer to center vertically
 vim.opt.showtabline = 2 -- Always show tabline
-vim.opt.sidescrolloff = 8 -- Columns of context
+vim.opt.sidescrolloff = 8 -- Keep cursor closer to center horizontally
 vim.opt.sidescroll = 1 -- Smoother horizontal scrolling
 vim.opt.signcolumn = 'yes:2' -- Sign gutter options: yes|no : int = size
 vim.opt.ignorecase = true -- Case insensitive search
-vim.opt.smartcase = true -- Don't ignore case with capitals
+vim.opt.smartcase = true -- Override search if pattern contains uppercase
 vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current
-vim.opt.termguicolors = true -- True color support
-vim.opt.timeout = true -- time limit for key sequences
-vim.opt.timeoutlen = 500 -- Wait for a mapped sequence
-vim.opt.ttimeoutlen = 0 -- Instant timeout for terminal buffer
-vim.opt.updatetime = 750 -- Time in milliseconds before update
-vim.opt.errorbells = false -- Produce noise on error
+vim.opt.termguicolors = true -- Enable true color support
+vim.opt.timeout = true -- Time limit for key sequences
+vim.opt.timeoutlen = 500 -- Timeout for mapped sequence
+vim.opt.ttimeoutlen = 0 -- Timeout for key code sequence
+vim.opt.updatetime = 750 -- Timeout idle before CursorHold or updating swapfile
+vim.opt.belloff = 'all' -- Events error bells will be suppressed
+vim.opt.errorbells = false -- Produce audible error feedback
 vim.opt.number = true -- Print line number
 vim.opt.relativenumber = true -- Print relative line numbers
 vim.opt.swapfile = false -- Disable swap files
@@ -80,7 +76,8 @@ vim.opt.undoreload = 10000 -- Save the whole buffer for undo
 vim.opt.wildmenu = true -- Command-line completion
 vim.opt.wildignorecase = true -- Case insensitive command-line completion
 
-vim.opt.formatoptions = 'tqnj' -- describes how auto-format is handled
+-- Describes how auto-format is handled
+vim.opt.formatoptions = 'tqnj'
 vim.opt.completeopt = 'menuone,noinsert,noselect' -- Insert mode completion
 
 -- Set what should be included in session files
@@ -93,32 +90,31 @@ vim.opt.whichwrap:append('<,>,[,],h,l')
 vim.opt.wildmode:append('full:lastused') -- Command-line completion mode
 vim.opt.wildignore:append({ -- Path patterns wild menu should ignore
   '**/.git/*', -- Git Version control
-  '**/android/*',
-  '**/build/*', -- Common build directory
-  '**/coverage/*',
-  '**/ios/*',
+  '**/android/*', -- Google Android build
+  '**/build/*', -- Generic build artifacts
+  '**/coverage/*', -- Test coverage reports
+  '**/ios/*', -- Apple iOS build
   '**/node_modules/*', -- Node module environment
-  '*.pyc', -- Python cached
-  '*_build/*',
+  '*.pyc', -- Python byte code
+  '*_build/*', -- Private build artifacts
 })
 
 -- Virtual Characters ---
 
 vim.opt.list = true
 vim.opt.listchars = {
-  -- eol = '↴',
-  space = ' ',
-  tab = '  ',
-  extends = '»',
-  multispace = '·',
-  precedes = '«',
-  trail = '·',
+  eol = ' ', -- End of line ↴
+  extends = '»', -- Horizontal overflow
+  multispace = '·', -- Consecutive whitespace
+  precedes = '«', -- Horizontal underflow
+  space = ' ', -- Standard Whitespace
+  tab = '  ', -- Tab indentation
+  trail = '·', -- Trailing whitespace
 }
 
 --- Windows ---
 
--- These settings apply only to nt/windows
-
+-- Options that only apply to nt/windows
 if vim.fn.has('win32') == 1 then
   vim.cmd([[
   " Set default shell to powershell

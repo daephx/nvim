23142896c86d35a4b4b4b17f876084a08edd3c1e
daephx <daephxdev@gmail.com>
2022-10-13 23:28:25 -0700

    refactor(packer): replace git conflict plugin
    Use newer lua implementation for highlighting and interacting with git
    merge conflicts.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -180,24 +180,8 @@ return require('packer').startup({
     })
 
     use({ -- Weapon to fight against conflicts in Vim.
-      'rhysd/conflict-marker.vim',
-      config = function()
-        vim.cmd([[
-        " disable the default highlight group
-        let g:conflict_marker_highlight_group = ''
-
-        " Include text after begin and end markers
-        let g:conflict_marker_begin = '^<<<<<<< .*$'
-        let g:conflict_marker_end   = '^>>>>>>> .*$'
-        let g:conflict_marker_separator = '^=======$'
-
-        highlight ConflictMarkerBegin guibg=#2f7366
-        highlight ConflictMarkerOurs guibg=#2e5049
-        highlight ConflictMarkerTheirs guibg=#344f69
-        highlight ConflictMarkerEnd guibg=#2f628e
-        highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
-        ]])
-      end,
+      'akinsho/git-conflict.nvim',
+      config = config('plugins.git-conflict'),
     })
 
     --- Language ---

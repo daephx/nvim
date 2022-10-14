1c6ac5f7aa203c5bc4d1e20e69245383801f5c07
daephx <daephxdev@gmail.com>
2022-10-13 23:12:19 -0700

    refactor(ex)!: rework custom bufdelete commands
    Command used to preserve window positions when deleting buffers.
    Set command to use `bdd` instead of overriding vim defaults.

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,24 +1,11 @@
--- Mistypes
 vim.cmd([[
+"" Mistypes
 cnoreabbrev W   w
 cnoreabbrev Wq wq
 cnoreabbrev Xa xa
 cnoreabbrev xA xa
 cnoreabbrev XA xa
 
-cnoreabbrev qq 1,$bd!
-
 " Better bufdelete
-cnoreabbrev bd bn\|bd#
-
-" Minimal BufDel
-" Prevent windows from being removed when deleting buffers
-function! BufferDelete()
-  if len(filter(range(1, bufnr('$')), 'buflisted(v:val)')) == 1
-    \ && &ft != 'dashboard'
-      Dashboard | bd#
-  else | bn | bd#
-  endif
-endfunction
-cnoreabbrev <silent> bd call BufferDelete()
+cnoreabbrev bdd bn\|bd#
 ]])

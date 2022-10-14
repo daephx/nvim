faabf85f96ace27c0905ae192ac0020ee15f900e
daephx <daephxdev@gmail.com>
2022-10-14 12:48:48 -0700

    refactor(nvim): improve `builtins` module
    - Cleanup document comments
    - Use table for `{builtin}_loaded` values

--- lua/core/builtins.lua
+++ lua/core/builtins.lua
@@ -3,42 +3,50 @@
 --   / __  / / / / / / __/ / __ \/ ___/
 --  / /_/ / /_/ / / / /_/ / / / (__  )
 -- /_____/\__,_/_/_/\__/_/_/ /_/____/
---
--- Module: plugins.builtins
--- Description: Configurations on how to handled builtin plugins
+-- ====================================
+-- Manage builtin vim runtime plugins.
+-- Disabling these can improve startuptime.
 
-local g = vim.g
-
---- Builtins ---
-
--- For now, you can just comment the items in this list
--- They will be disable if they are NOT commented.
-local disabled_plugins = {
-  '2html_plugin',
-  'getscript',
-  'getscriptPlugin',
-  'gzip',
-  'logiPat',
-  'matchit',
-  -- 'matchparen',
-  -- "netrw",
-  -- "netrwFileHandlers",
-  -- "netrwPlugin",
-  -- "netrwSettings",
-  'rrhelper',
-  -- "spellfile_plugin",
-  'tar',
-  'tarPlugin',
-  'vimball',
-  'vimballPlugin',
-  'zip',
-  'zipPlugin',
+local builtins = {
+  did_indent_on = 1,
+  did_load_ftplugin = 1,
+  loaded_2html_plugin = 1,
+  loaded_bugreport = 1,
+  loaded_compiler = 1,
+  loaded_ftplugin = 1,
+  loaded_getscript = 1,
+  loaded_getscriptPlugin = 1,
+  loaded_gzip = 1,
+  loaded_logiPat = 1,
+  loaded_matchit = 1,
+  loaded_matchparen = 0,
+  loaded_netrw = 0,
+  loaded_netrwFileHandlers = 0,
+  loaded_netrwPlugin = 0,
+  loaded_netrwSettings = 0,
+  loaded_node_provider = 1,
+  loaded_optwin = 1,
+  loaded_perl_provider = 1,
+  loaded_python3_provider = 1,
+  loaded_python_provider = 1,
+  loaded_rplugin = 1,
+  loaded_rrhelper = 1,
+  loaded_ruby_provider = 1,
+  loaded_spellfile_plugin = 0,
+  loaded_synmenu = 1,
+  loaded_syntax = 1,
+  loaded_tar = 1,
+  loaded_tarPlugin = 1,
+  loaded_tutor = 1,
+  loaded_vimball = 1,
+  loaded_vimballPlugin = 1,
+  loaded_zip = 1,
+  loaded_zipPlugin = 1,
 }
 
--- Set variable that tricks vim into thinking
--- the plugin has already been loaded
-for _, plugin in pairs(disabled_plugins) do
-  g['loaded_' .. plugin] = 1
+-- Set global variables
+for k, v in pairs(builtins) do
+  vim.g[k] = v
 end
 
 --- Settings ---
@@ -46,11 +54,9 @@ end
 -- For the plugins that are still enabled
 -- Apply your preferred settings
 
--- Netrw
-
-if g.loaded_netrwPlugin then
-  g.netrw_banner = 0
-  g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
-  g.netrw_liststyle = 3 -- Default to tree view
-  g.netrw_localrmdir = 'rm -r'
+if not vim.g.loaded_netrwPlugin then
+  vim.g.netrw_banner = 0
+  vim.g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
+  vim.g.netrw_liststyle = 3 -- Default to tree view
+  vim.g.netrw_localrmdir = 'rm -r'
 end

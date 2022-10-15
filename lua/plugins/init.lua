1cc4a4f14ebca4675561a3f63e6358900b89c676
daephx <daephxdev@gmail.com>
2022-10-14 23:07:22 -0700

    refactor(packer): update plugin definitions for markdown-preview
    - Use new `run` command from project README example
    
    - Disable autostart
    - Keep filename enabled
    - Prefer dark theme
    - Show yaml metadata

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -251,9 +251,16 @@ return require('packer').startup({
     use({ -- Preview Markdown in the browser
       'iamcco/markdown-preview.nvim',
       ft = 'markdown',
-      run = 'call mkdp#util#install()',
+      run = function()
+        vim.fn['mkdp#util#install']()
+      end,
       config = function()
-        vim.g.mkdp_auto_start = 1
+        vim.g.mkdp_auto_start = 0
+        vim.g.mkdp_preview_options = {
+          hide_yaml_meta = 0,
+          Disable_filename = 0,
+          ['prefers-color-scheme'] = 'dark',
+        }
       end,
     })
 

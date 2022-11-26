c86082c34b15220a51d6ca2a7520a00a4c37bdb4
daephx <daephxdev@gmail.com>
2022-11-26 06:38:06 -0800

    refactor(lsp): return capabilities from local module
    Instead of returning a table with function definition for initializing
    capabilities, just initialize them and return from module.

--- lua/lsp/capabilities.lua
+++ lua/lsp/capabilities.lua
@@ -1,23 +1,18 @@
-local M = {}
-
 -- Initialize LSP capabilities
----@return table
-M.initialize_capabilities = function()
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  local cmp_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-  if cmp_ok then
-    capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
-  end
 
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-  capabilities.textDocument.completion.completionItem.resolveSupport = {
-    properties = {
-      'additionalTextEdits',
-      'detail',
-      'documentation',
-    },
-  }
-  return capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+local cmp_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+if cmp_ok then
+  capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 end
 
-return M
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    'additionalTextEdits',
+    'detail',
+    'documentation',
+  },
+}
+
+return capabilities

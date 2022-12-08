26af404daf09236ced8926b18b6e7f616e65e1fe
daephx <daephxdev@gmail.com>
2022-12-08 07:45:33 -0800

    feat: add plugin nvim-unception, prevent nested instance
    This plugin uses the Neovim RPC to open files in the current instance.
    It's helpful for opening files from terminal buffers via `nvim
    README.md` without a new instance of Neovim being spawned within the
    active terminal buffer.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -351,6 +351,23 @@ return require('packer').startup({
       end,
     })
 
+    use({ -- Prevent nested instances of neovim
+      'samjwill/nvim-unception',
+      config = function()
+        vim.g.unception_enable_flavor_text = false
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'UnceptionEditRequestReceived',
+          callback = function()
+            local wins = vim.api.nvim_list_wins()
+            if #wins < 1 then
+              vim.cmd('topleft split')
+            end
+            vim.api.nvim_set_current_win(wins[1])
+          end,
+        })
+      end,
+    })
+
     -- ======================================
     -- *       END OF PLUGIN SECTION       *
     -- ======================================

bdd096d529bbeb9469dae62246eb370ce25ea3c8
daephx <daephxdev@gmail.com>
2023-01-26 00:32:40 -0800

    refactor: remove wrapper func for telescope setup
    Call setup directory from file instead of wrapping in module function.
    Previously helpful for loading extensions to limit errors, but plugin
    extensions should handle their own initialization.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,10 @@
--- Module: plugins.telescope
--- Description: configurations for telescope fuzzy-finder
+-- Telescope config | configurations for telescope fuzzy-finder
+-- https://github.com/nvim-telescope/telescope.nvim
+local telescope_ok, telescope = pcall(require, "telescope")
+if not telescope_ok then
+  return
+end
 
-local telescope = require("telescope")
 local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
 local previewers = require("telescope.previewers")
@@ -13,138 +16,136 @@ local action_set = require("telescope.actions.set")
 
 local M = {}
 
-M.config = function()
-  telescope.setup({
-    defaults = { -- Default configuration for telescope goes here:
-      prompt_prefix = "> ",
-      color_devicons = true,
-      dynamic_preview_title = true,
-      -- layout_strategy= 'flex',
-      -- scroll_strategy = 'cycle',
-      scroll_strategy = "limit",
-      selection_strategy = "row",
-      sorting_strategy = "ascending",
-      winblend = 5,
+telescope.setup({
+  -- Default configuration for telescope goes here:
+  defaults = {
+    prompt_prefix = "> ",
+    color_devicons = true,
+    dynamic_preview_title = true,
+    -- layout_strategy= 'flex',
+    -- scroll_strategy = 'cycle',
+    scroll_strategy = "limit",
+    selection_strategy = "row",
+    sorting_strategy = "ascending",
+    winblend = 5,
 
-      -- file_sorter = sorters.get_fzy_sorter,
-      file_sorter = sorters.get_fuzzy_file,
-      file_previewer = previewers.vim_buffer_cat.new,
-      grep_previewer = previewers.vim_buffer_vimgrep.new,
-      qflist_previewer = previewers.vim_buffer_qflist.new,
+    -- file_sorter = sorters.get_fzy_sorter,
+    file_sorter = sorters.get_fuzzy_file,
+    file_previewer = previewers.vim_buffer_cat.new,
+    grep_previewer = previewers.vim_buffer_vimgrep.new,
+    qflist_previewer = previewers.vim_buffer_qflist.new,
 
-      vimgrep_arguments = {
-        "rg",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
+    vimgrep_arguments = {
+      "rg",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    },
+
+    -- Set character list for prompt borders
+    border = {},
+    -- borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' }, -- Rounded corners
+    borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" }, -- Square Corners
+
+    layout_config = { -- Default settings for layout themes
+      prompt_position = "top",
+      horizontal = {
+        width = 0.9,
+        height = 0.95,
+        preview_width = 0.55,
       },
-
-      -- Set character list for prompt borders
-      border = {},
-      -- borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' }, -- Rounded corners
-      borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" }, -- Square Corners
-
-      layout_config = { -- Default settings for layout themes
-        prompt_position = "top",
-        horizontal = {
-          width = 0.9,
-          height = 0.95,
-          preview_width = 0.55,
-        },
-        vertical = {
-          width_padding = 0.05,
-          height_padding = 1,
-          preview_height = 0.5,
-        },
+      vertical = {
+        width_padding = 0.05,
+        height_padding = 1,
+        preview_height = 0.5,
       },
+    },
 
-      mappings = { -- Custom mappings for telescope prompt
+    mappings = { -- Custom mappings for telescope prompt
+      i = {
+        ["qq"] = actions.close,
+        ["<C-u>"] = false,
+        ["<C-d>"] = false,
+        ["<C-h>"] = "which_key",
+        ["<C-PageUp>"] = "preview_scrolling_up",
+        ["<C-PageDown>"] = "preview_scrolling_down",
+
+        -- Scroll results by half/page
+        ["<PageUp>"] = function(prompt_bufnr)
+          local results_win = state.get_status(prompt_bufnr).results_win
+          local height = vim.api.nvim_win_get_height(results_win)
+          action_set.shift_selection(prompt_bufnr, -math.floor(height / 2))
+        end,
+        ["<PageDown>"] = function(prompt_bufnr)
+          local results_win = state.get_status(prompt_bufnr).results_win
+          local height = vim.api.nvim_win_get_height(results_win)
+          action_set.shift_selection(prompt_bufnr, math.floor(height / 2))
+        end,
+      },
+      n = {
+        ["<esc>"] = actions.close,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+      },
+    },
+
+    file_ignore_patterns = { -- File patterns to exclude from fuzzy results
+      "*.pyc", -- Python cache
+      "*.git/.*", -- Git version control directory
+      ".pytest_cache/.*", -- Pytest unittest cache files
+      "__pycache__/.*", -- Python cache files
+      "_build/.*",
+      "build/.*",
+      "node_modules/.*", -- Node module environment
+      "src/parser.c",
+      "tags/.*",
+    },
+  },
+  pickers = { -- Default configuration for builtin pickers goes here:
+
+    -- Now the picker_config_key will be applied every time you call this
+    buffers = {
+      sort_mru = true,
+      theme = "ivy",
+      selection_strategy = "closest",
+      mappings = {
         i = {
-          ["qq"] = actions.close,
-          ["<C-u>"] = false,
-          ["<C-d>"] = false,
-          ["<C-h>"] = "which_key",
-          ["<C-PageUp>"] = "preview_scrolling_up",
-          ["<C-PageDown>"] = "preview_scrolling_down",
-
-          -- Scroll results by half/page
-          ["<PageUp>"] = function(prompt_bufnr)
-            local results_win = state.get_status(prompt_bufnr).results_win
-            local height = vim.api.nvim_win_get_height(results_win)
-            action_set.shift_selection(prompt_bufnr, -math.floor(height / 2))
-          end,
-          ["<PageDown>"] = function(prompt_bufnr)
-            local results_win = state.get_status(prompt_bufnr).results_win
-            local height = vim.api.nvim_win_get_height(results_win)
-            action_set.shift_selection(prompt_bufnr, math.floor(height / 2))
-          end,
-        },
-        n = {
-          ["<esc>"] = actions.close,
-          ["<C-j>"] = actions.move_selection_next,
-          ["<C-k>"] = actions.move_selection_previous,
-          ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+          ["<c-d>"] = actions.delete_buffer,
         },
       },
-
-      file_ignore_patterns = { -- File patterns to exclude from fuzzy results
-        "*.pyc", -- Python cache
-        "*.git/.*", -- Git version control directory
-        ".pytest_cache/.*", -- Pytest unittest cache files
-        "__pycache__/.*", -- Python cache files
-        "_build/.*",
-        "build/.*",
-        "node_modules/.*", -- Node module environment
-        "src/parser.c",
-        "tags/.*",
-      },
     },
-    pickers = { -- Default configuration for builtin pickers goes here:
-
-      -- Now the picker_config_key will be applied every time you call this
-      buffers = {
-        sort_mru = true,
-        theme = "ivy",
-        selection_strategy = "closest",
-        mappings = {
-          i = {
-            -- ['<c-w>'] =
-            ["<c-d>"] = actions.delete_buffer,
-          },
-        },
-      },
-      man_pages = { sections = { "2", "3" } },
-      lsp_code_actions = { theme = "ivy" },
-      lsp_document_diagnostics = { theme = "ivy" },
-      lsp_document_symbols = { theme = "ivy", path_display = { "hidden" } },
-      lsp_references = { theme = "ivy", path_display = { "shorten" } },
-      lsp_workspace_symbols = { theme = "ivy", path_display = { "shorten" } },
-      colorscheme = { theme = "dropdown", previewer = false },
-      file_browser = { theme = "ivy" },
+    man_pages = { sections = { "2", "3" } },
+    lsp_code_actions = { theme = "ivy" },
+    lsp_document_diagnostics = { theme = "ivy" },
+    lsp_document_symbols = { theme = "ivy", path_display = { "hidden" } },
+    lsp_references = { theme = "ivy", path_display = { "shorten" } },
+    lsp_workspace_symbols = { theme = "ivy", path_display = { "shorten" } },
+    colorscheme = { theme = "dropdown", previewer = false },
+    file_browser = { theme = "ivy" },
+  },
+  extensions = { -- Your extension configuration goes here:
+    -- please take a look at the readme of the extension you want to configure
+    fzf = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or 'ignore_case' or 'respect_case'
+      -- the default case_mode is 'smart_case'
     },
-    extensions = { -- Your extension configuration goes here:
-      -- please take a look at the readme of the extension you want to configure
-      fzf = {
-        fuzzy = true, -- false will only do exact matching
-        override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = "smart_case", -- or 'ignore_case' or 'respect_case'
-        -- the default case_mode is 'smart_case'
-      },
-      media_files = {
-        -- filetypes whitelist
-        -- defaults to {'png', 'jpg', 'mp4', 'webm', 'pdf'}
-        filetypes = { "png", "webp", "jpg", "jpeg" },
-        find_cmd = "rg", -- find command (defaults to `fd`)
-      },
+    media_files = {
+      -- filetypes whitelist
+      -- defaults to {'png', 'jpg', 'mp4', 'webm', 'pdf'}
+      filetypes = { "png", "webp", "jpg", "jpeg" },
+      find_cmd = "rg", -- find command (defaults to `fd`)
     },
-  })
+  },
+})
 
-  -- Highlights
-  vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-end
+-- Highlights
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
 
 --- Extensions ---
 

8c0d6a1c6a73856ef8611dcc96423dbcd2742dec
daephx <daephxdev@gmail.com>
2023-01-28 05:46:02 -0800

    fix: manpage buffers unlisted and delete when hidden

--- after/ftplugin/man.lua
+++ after/ftplugin/man.lua
@@ -1,5 +1,7 @@
 -- after/ftplugin/man
 
+vim.opt_local.buflisted = false
+vim.opt_local.bufhidden = "delete"
 vim.opt_local.spell = false
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false

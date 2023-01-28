feff90ba5ac5cf3bed96234c9caa45b263c28c52
daephx <daephxdev@gmail.com>
2023-01-28 05:39:21 -0800

    refactor: rename core module `utils` -> `globals`
    Better represents what the module is primary being used for.
    
    Utilities will likely be defined per plugin or generally in the runtime
    scope as `require("utils")`.

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -1,7 +1,10 @@
--- Module: utils
--- Description: Utility module
-
---- Globals ---
+--    ________      __          __
+--   / ____/ /___  / /_  ____ _/ /____
+--  / / __/ / __ \/ __ \/ __ `/ / ___/
+-- / /_/ / / /_/ / /_/ / /_/ / (__  )
+-- \____/_/\____/_.___/\__,_/_/____/
+-- =================================
+-- Set global variables & functions
 
 -- Easier Inspect lua objects
 function _G.inspect(args)
@@ -43,31 +46,3 @@ function _G.map(mode, lhs, rhs, opts)
   end
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
-
---- Module ---
-
-local M = {}
-
--- Generate array from file lines
-function M.fileToArray(fpath)
-  local file = io.open(fpath, "r")
-  local arr = {}
-  for line in file:lines() do
-    table.insert(arr, line)
-  end
-  return arr
-end
-
--- Math operations
--- Check if number is prime
-function M.is_prime(n)
-  local max_divider_checked = math.sqrt(n) + 1
-  for i = 2, max_divider_checked do
-    if n % i == 0 then
-      return false
-    end
-  end
-  return true
-end
-
-return M
--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,4 +1,4 @@
-require("core.utils")
+require("core.globals")
 require("core.options")
 require("core.filetypes")
 require("core.builtins")

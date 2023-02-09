1559c96d43272c76d6d08b24b1c7ad12b8f0e278
daephx <daephxdev@gmail.com>
2023-02-08 20:15:38 -0800

    docs: cleanup autocmd:desc for lsp_document_highlight
    - Fix spelling mistakes and improve wording

--- lua/plugins/lsp/handlers/highlight.lua
+++ lua/plugins/lsp/handlers/highlight.lua
@@ -7,7 +7,7 @@ M.enable_document_highlighting = function(client, bufnr)
   if client.supports_method("textDocument/documentHighlight") then
     local augroup = vim.api.nvim_create_augroup("LspDocumentHighlight", { clear = true })
     vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
-      desc = "Link LSP document reference highlighting to IncSearch",
+      desc = "Link LSP document reference highlight groups to IncSearch",
       group = augroup,
       callback = function()
         local opts = { link = "IncSearch", bold = true }
@@ -18,13 +18,13 @@ M.enable_document_highlighting = function(client, bufnr)
     })
     vim.api.nvim_create_autocmd({ "CursorHold" }, {
       buffer = bufnr,
-      desc = "Apply LSP documment reference highlighting on CursorHold",
+      desc = "Apply LSP document reference highlighting on CursorHold",
       group = augroup,
       callback = vim.lsp.buf.document_highlight,
     })
     vim.api.nvim_create_autocmd({ "CursorMoved", "InsertEnter", "CursorMovedI" }, {
       buffer = bufnr,
-      desc = "Disable LSP documment reference highlighting on CursorMoved",
+      desc = "Disable LSP document reference highlighting on CursorMoved",
       group = augroup,
       callback = vim.lsp.buf.clear_references,
     })

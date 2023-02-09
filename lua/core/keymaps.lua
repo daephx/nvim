0a4578036fa40e949f443a3e0d4ce85508a2b8d7
daephx <daephxdev@gmail.com>
2023-02-09 06:31:50 -0800

    refactor: remove whichkey map from `core/keymaps`
    Use `vim.keymap` api instead of whichkey api for applying keymaps due to
    the order in which core configs and plugins are loaded.
    
    Core configs should be loaded before lazy, but calling whichkey before
    lazy is loaded results in missing keybindings or errors if not using
    `pcall(require, ...)`.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -39,90 +39,93 @@ map("n", "<localleader>q", "<cmd>call ToggleQFList(1)<CR>")
 map("n", "<c-k>", "<cmd>cprev<CR>zz")
 map("n", "<c-j>", "<cmd>cnext<CR>zz")
 
---- Whichkey ---
-
--- Check for whichkey plugin
-local ok, wk = pcall(require, "which-key")
-if not ok then
-  return
-end
-
-local whichkey_map = {}
-
 -- General
-whichkey_map = {
-  name = "+Whichkey",
-  ["<Space>"] = { "<cmd>Telescope buffers<CR>", "Open Buffers" },
-  ["?"] = { "Cheatsheet" },
-  ["/"] = { "<Plug>kommentary_line_default", "Comment Line" },
-  P = { "which_key_ignore" },
-  e = { "<cmd>NvimTreeToggle<cr>", "Toggle NvimTree" },
-  q = { "LSP Set Location List" },
-}
+map("n", "<leader><Space>", "<cmd>Telescope buffers<CR>", { desc = "Open Buffers" })
+map("n", "<leader>/", "<Plug>kommentary_line_default", { desc = "Comment Line" })
+map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
 
 -- Debug Protocol
-whichkey_map.d = {
-  name = "+Debug",
-  b = { '<cmd>lua require("dap").toggle_breakpoint()<CR>', "Set Breakpoint" },
-  d = { '<cmd>lua require("dap.python").attach_python_debugger()<CR>', "Start Debugging" },
-  n = { '<cmd>lua require("dap").continue()<CR>', "Continue debugging" },
-}
+map("n", "<leader>db", '<cmd>lua require("dap").toggle_breakpoint()<CR>', { desc = "Set Breakpoint" })
+map(
+  "n",
+  "<leader>dd",
+  '<cmd>lua require("dap.python").attach_python_debugger()<CR>',
+  { desc = "Start Debugging" }
+)
+map("n", "<leader>dn", '<cmd>lua require("dap").continue()<CR>', { desc = "Continue debugging" })
 
 -- Telescope Find
-whichkey_map.f = {
-  name = "+Files",
-  ["."] = { '<cmd>lua require("plugins.telescope").search_dotfiles()<CR>', "Dotfiles" },
-  ["<S-c>"] = { "<cmd>Telescope commands<CR>", "Commands" },
-  ["<S-f>"] = { "<cmd>Telescope treesitter<CR>", "Treesitter" },
-  b = { "<cmd>Telescope current_buffer_fuzzy_find<CR>", "Grep Buffer" },
-  c = { "<cmd>Telescope colorscheme<CR>", "ColorSchemes" },
-  d = { "<cmd>Telescope lsp_document_diagnostics<CR>", "LSP Diagnostics" },
-  e = { "<cmd>Telescope file_browser<CR>", "File Explorer" },
-  f = { "<cmd>Telescope find_files<CR>", "Current Files" },
-  h = { "<cmd>Telescope highlights<CR>", "Highlight Groups" },
-  l = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
-  m = { "<cmd>Telescope marks<CR>", "Bookmarks" },
-  p = { "<cmd>Telescope projects<CR>", "Project list" },
-  r = { "<cmd>Telescope oldfiles<CR>", "Recent Files" },
-  s = { "<cmd>Telescope grep_string<CR>", "Grep String" },
-  t = { "<cmd>TodoTelescope<CR>", "Search Todo comments" },
-  v = { '<cmd>lua require("plugins.telescope").search_vimfiles()<CR>', "VimRC" },
-  w = { '<cmd>lua require("plugins.telescope").search_wikifiles()<CR>', "Wiki" },
-}
+map(
+  "n",
+  "<leader>f.",
+  '<cmd>lua require("plugins.telescope").search_dotfiles()<CR>',
+  { desc = "Dotfiles" }
+)
+map("n", "<leader>f<S-c>", "<cmd>Telescope commands<CR>", { desc = "Commands" })
+map("n", "<leader>f<S-f>", "<cmd>Telescope treesitter<CR>", { desc = "Treesitter" })
+map("n", "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "Grep Buffer" })
+map("n", "<leader>fc", "<cmd>Telescope colorscheme<CR>", { desc = "ColorSchemes" })
+map("n", "<leader>fd", "<cmd>Telescope lsp_document_diagnostics<CR>", { desc = "LSP Diagnostics" })
+map("n", "<leader>fe", "<cmd>Telescope file_browser<CR>", { desc = "File Explorer" })
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Current Files" })
+map("n", "<leader>fh", "<cmd>Telescope highlights<CR>", { desc = "Highlight Groups" })
+map("n", "<leader>fl", "<cmd>Telescope live_grep<CR>", { desc = "Live Grep" })
+map("n", "<leader>fm", "<cmd>Telescope marks<CR>", { desc = "Bookmarks" })
+map("n", "<leader>fp", "<cmd>Telescope projects<CR>", { desc = "Project list" })
+map("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Recent Files" })
+map("n", "<leader>fs", "<cmd>Telescope grep_string<CR>", { desc = "Grep String" })
+map("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Search Todo comments" })
+map(
+  "n",
+  "<leader>fv",
+  '<cmd>lua require("plugins.telescope").search_vimfiles()<CR>',
+  { desc = "VimRC" }
+)
+map(
+  "n",
+  "<leader>fw",
+  '<cmd>lua require("plugins.telescope").search_wikifiles()<CR>',
+  { desc = "Wiki" }
+)
 
 -- Plugins
-whichkey_map.p = {
-  name = "+Plugins",
-  s = { "<cmd>Lazy show<CR>", "Plugins status" },
-  u = { "<cmd>Lazy update<CR>", "Update Plugins" },
-}
+map("n", "<leader>ps", "<cmd>Lazy show<CR>", { desc = "Plugins status" })
+map("n", "<leader>pu", "<cmd>Lazy update<CR>", { desc = "Update Plugins" })
 
 -- Git
-whichkey_map.g = {
-  name = "+Git",
-  G = { '<cmd>lua require("neogit").open()<CR>', "Git Status (Neogit)" },
-  b = { '<cmd>lua require("plugins.telescope").git_branches()<CR>', "Git branches" },
-  d = { "<cmd>Gdiffsplit<CR>", "Diff current buffer" },
-  f = { "<cmd>Telescope git_files<CR>", "Git files" },
-  g = { "<cmd>Gtabedit :<CR>", "Git Status (Fugitive)" },
-  l = { "<cmd>GclogTab<CR>", "Git Log" },
-  z = { '<cmd>lua require("plugins.toggleterm").lazygit:toggle()<CR>', "Lazygit" },
-}
+map("n", "<leader>gG", '<cmd>lua require("neogit").open()<CR>', { desc = "Git Status (Neogit)" })
+map(
+  "n",
+  "<leader>gb",
+  '<cmd>lua require("plugins.telescope").git_branches()<CR>',
+  { desc = "Git branches" }
+)
+map("n", "<leader>gd", "<cmd>Gdiffsplit<CR>", { desc = "Diff current buffer" })
+map("n", "<leader>gf", "<cmd>Telescope git_files<CR>", { desc = "Git files" })
+map("n", "<leader>gg", "<cmd>Gtabedit :<CR>", { desc = "Git Status (Fugitive)" })
+map("n", "<leader>gl", "<cmd>GclogTab<CR>", { desc = "Git Log" })
+map(
+  "n",
+  "<leader>gz",
+  '<cmd>lua require("plugins.toggleterm").lazygit:toggle()<CR>',
+  { desc = "Lazygit" }
+)
 
 -- New
-whichkey_map.n = {
-  name = "+New",
-  t = { "<cmd>tabnew<CR>", "New tab" },
-  f = { "<cmd>enew<CR>", "New file" },
-}
+map("n", "<leader>nt", "<cmd>tabnew<CR>", { desc = "New tab" })
+map("n", "<leader>nf", "<cmd>enew<CR>", { desc = "New file" })
 
 -- Terminal
-whichkey_map.t = {
-  name = "+Terminal",
-  t = { "<cmd>ToggleTerm direction=tab<CR>", "Terminal Tab" },
-  d = { '<cmd>lua require("plugins.toggleterm").lazydocker:toggle()<CR>', "Lazydocker" },
-  g = { '<cmd>lua require("plugins.toggleterm").lazygit:toggle()<CR>', "Lazygit" },
-}
-
--- Register Keymap
-wk.register(whichkey_map, { prefix = "<Leader>" })
+map("n", "<leader>tt", "<cmd>ToggleTerm direction=tab<CR>", { desc = "Terminal Tab" })
+map(
+  "n",
+  "<leader>td",
+  '<cmd>lua require("plugins.toggleterm").lazydocker:toggle()<CR>',
+  { desc = "Lazydocker" }
+)
+map(
+  "n",
+  "<leader>tg",
+  '<cmd>lua require("plugins.toggleterm").lazygit:toggle()<CR>',
+  { desc = "Lazygit" }
+)

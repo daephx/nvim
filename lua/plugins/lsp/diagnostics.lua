8fc27e13cfd25b275822c4e860294a7e2795cf1f
daephx <daephxdev@gmail.com>
2023-02-08 19:44:16 -0800

    refactor: cleanup lsp/diagnostics config
    - Add document comments and type hints
    - Cleanup minor formatting inconsistencies
    - Rename client rename table to `client_map`

--- lua/plugins/lsp/diagnostics.lua
+++ lua/plugins/lsp/diagnostics.lua
@@ -1,33 +1,40 @@
 -- lsp/handlers/diagnostics.lua
-
 local icons = require("plugins.lsp.icons")
 
-local sources = {
+local client_map = {
   { "Lua Diagnostics.", "Sumneko" },
   { "Lua Syntax Check.", "Sumneko" },
 }
 
 -- Allow for replacing diagnostic source from matching name
+---@param diagnostic table
+---@return string
 local rename_diagnostic_source = function(diagnostic)
-  for _, item in pairs(sources) do
+  for _, item in pairs(client_map) do
     if diagnostic.source == item[1] then
       return item[2]
     end
   end
-
   return diagnostic.source
 end
 
+-- Format diagnostic virtual text message
+---@param diagnostic table
+---@return string
 local format = function(diagnostic)
   local name = rename_diagnostic_source(diagnostic)
   return string.format("%s: %s ", name, diagnostic.message)
 end
 
+-- Format diagnostic floating window prefix
+---@param diagnostic table
+---@return string, string
 local float_prefix = function(diagnostic)
   local tbl = icons.diagnostics[diagnostic.severity]
   return string.format("%s ", tbl.text), tbl.hl
 end
 
+-- Apply neovim lsp diagnostic configs
 vim.diagnostic.config({
   signs = true,
   underline = true,

bd5c2c076484e111d8f3ec4bba005ebe8753f6c8
daephx <daephxdev@gmail.com>
2023-02-08 19:29:10 -0800

    feat: add user command to list active null-ls sources
    Add user command to list null-ls sources that are active in the current buffer.

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -87,6 +87,14 @@ null_ls.setup({
   },
 })
 
+-- Get list of null-ls sources
+vim.api.nvim_create_user_command("NullLsSources", function()
+  local sources = require("null-ls.sources")
+  print(vim.inspect(vim.tbl_map(function(source)
+    return source.name
+  end, sources.get_available(vim.bo.filetype))))
+end, {})
+
 -- Override window options for null-ls info
 vim.api.nvim_create_autocmd("FileType", {
   desc = "Override window options for null-ls info",

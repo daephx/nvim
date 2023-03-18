5ad3046dad90bd3a31c700417f55f972cdc5b865
daephx <daephxdev@gmail.com>
2023-03-17 22:59:24 -0700

    fix: use `vim.cmd` method w/ more command abbrevs
    - Replace vimscript multi line string command with `vim.cmd` method.
    - Add more variants to cnoreabbrev commands to help when fat fingering
      the shift button with `bd` or `wq`.

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,11 +1,34 @@
-vim.cmd([[
-"" Mistypes
-cnoreabbrev W   w
-cnoreabbrev Wq wq
-cnoreabbrev Xa xa
-cnoreabbrev xA xa
-cnoreabbrev XA xa
+-- Commands
 
-" Better bufdelete
-cnoreabbrev bdd bn\|bd#
-]])
+-- Typo commands
+vim.cmd.cnoreabbrev({ "BD", "bd" })
+vim.cmd.cnoreabbrev({ "Bd", "bd" })
+vim.cmd.cnoreabbrev({ "bD", "bd" })
+
+vim.cmd.cnoreabbrev({ "E", "e" })
+
+vim.cmd.cnoreabbrev({ "SO", "so" })
+vim.cmd.cnoreabbrev({ "So", "so" })
+vim.cmd.cnoreabbrev({ "sO", "so" })
+
+vim.cmd.cnoreabbrev({ "Q", "q" })
+vim.cmd.cnoreabbrev({ "QA", "qa" })
+vim.cmd.cnoreabbrev({ "Qa", "qa" })
+vim.cmd.cnoreabbrev({ "qA", "qa" })
+
+vim.cmd.cnoreabbrev({ "SO", "so" })
+vim.cmd.cnoreabbrev({ "So", "so" })
+vim.cmd.cnoreabbrev({ "sO", "so" })
+
+vim.cmd.cnoreabbrev({ "W", "w" })
+vim.cmd.cnoreabbrev({ "WQ", "wq" })
+vim.cmd.cnoreabbrev({ "Wq", "wq" })
+vim.cmd.cnoreabbrev({ "wQ", "wq" })
+
+vim.cmd.cnoreabbrev({ "X", "x" })
+vim.cmd.cnoreabbrev({ "XA", "xa" })
+vim.cmd.cnoreabbrev({ "Xa", "xa" })
+vim.cmd.cnoreabbrev({ "xA", "xa" })
+
+-- Delete buffer without closing window
+vim.cmd.cnoreabbrev({ "bdd", "bn|bd#" })

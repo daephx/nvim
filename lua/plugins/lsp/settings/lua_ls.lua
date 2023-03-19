aee8cee996581873589df784b4177f6ea59d49c4
daephx <daephxdev@gmail.com>
2023-03-19 01:24:45 -0700

    refactor!: update lua_ls configurations
    refactor!: disable lua_ls formatting, remove complex configs
    
    Formatting typically provided by null-ls / stylua for lua documents.
    Applying stylua with lua_ls formatting rules can result in many style
    errors.
    
    Remove extraneous settings from default config, should this be enabled
    again in the future. Settings can either be defined in editorconfig or
    reintroduced from the same file.
    
    feat: include lazy directory in lua_ls workspace
    
    Allow plugin directories to be read via lua_ls.
    Increase the max preload but limit preload file size for large files.
    
    This will increase load times for lua_ls but have the benefit of lsp
    related functions such as signature_help and proper type aliases for
    plugin references.
    
    ---
    
    A consideration for better handling of this could be to conditionally
    apply this workspace folder only if the current working directory is
    `stdpath("config")`. Such that, when working on lua projects that are
    NOT part of managing neovim configurations, these folders are not needed
    and will not be loaded.
    
    refactor: ignore underscore labels from unused warning for lua_ls
    
    Local functions and variables that are prepended with an underscore `_*`
    will not display a style/linting error provided by lua_ls when normally
    applicable.
    
    refactor: set snippet completion to replace in lua_ls
    
    refactor: add explicit rules for style checking in lua_ls

--- lua/plugins/lsp/settings/lua_ls.lua
+++ lua/plugins/lsp/settings/lua_ls.lua
@@ -11,11 +11,30 @@ return {
         version = "LuaJIT",
         path = runtime_path,
       },
+      completion = { callSnippet = "Replace" },
       diagnostics = {
         enable = true,
-        neededFileStatus = {
-          ["codestyle-check"] = "Any",
+        groupSeverity = {
+          strong = "Warning",
+          strict = "Warning",
         },
+        groupFileStatus = {
+          ["ambiguity"] = "Opened",
+          ["await"] = "Opened",
+          ["codestyle"] = "None",
+          ["duplicate"] = "Opened",
+          ["global"] = "Opened",
+          ["luadoc"] = "Opened",
+          ["redefined"] = "Opened",
+          ["strict"] = "Opened",
+          ["strong"] = "None",
+          -- ["strong"] = "Opened",
+          ["type-check"] = "Opened",
+          ["unbalanced"] = "Opened",
+          ["unused"] = "Opened",
+        },
+        unusedLocalExclude = { "_*" },
+        disable = { "lowercase-global" },
         globals = {
           "after_each",
           "before_each",
@@ -25,35 +44,21 @@ return {
           "teardown",
           "vim",
         },
-        disable = { "lowercase-global" },
       },
       format = {
-        enable = true,
+        enable = false,
         defaultConfig = {
-          indent_style = vim.o.expandtab and "space" or "tab",
-          indent_size = tostring(vim.o.shiftwidth),
-          max_line_length = tostring(vim.o.textwidth),
+          indent_size = "2",
+          indent_style = "space",
+          insert_final_newline = "true",
+          max_line_length = "80",
           quote_style = "double",
           trim_trailing_whitespace = "true",
-          insert_final_newline = "true",
-          keep_line_after_do_statement = "maxLine:1",
-          keep_line_after_expression_statement = "maxLine:1",
-          keep_line_after_for_statement = "maxLine:1",
-          keep_line_after_function_define_statement = "maxLine:1",
-          keep_line_after_if_statement = "maxLine:1",
-          keep_line_after_local_or_assign_statement = "keepLine",
-          keep_line_after_repeat_statement = "maxLine:1",
-          keep_line_after_while_statement = "maxLine:1",
-          keep_one_space_between_call_args_and_parentheses = "false",
-          keep_one_space_between_namedef_and_attribute = "true",
-          keep_one_space_between_table_and_bracket = "true",
-          max_continuous_line_distance = "0",
-          table_append_expression_no_space = "true",
         },
       },
       workspace = {
-        maxPreload = 2000,
-        preloadFileSize = 50000,
+        maxPreload = 100000,
+        preloadFileSize = 10000,
         checkThirdParty = false,
         library = {
           -- Add default runtime files to library
@@ -62,6 +67,9 @@ return {
 
           -- Add user config to library
           [vim.fn.stdpath("config") .. "/lua"] = true,
+
+          -- Add lazy plugin directory
+          [vim.fn.stdpath("data") .. "/lazy"] = true,
         },
       },
       telemetry = {

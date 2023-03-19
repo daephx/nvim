2f28cb3bdf214d52f22a6ec1c36695cf8fdb0765
daephx <daephxdev@gmail.com>
2023-03-18 20:26:57 -0700

    refactor: remove unused telescope extensions options
    Default config for telescope extensions defined here are not utilized or
    even installed. Other extensions handle applying settings via subsequent
    calls to `telescope.setup` to extend table values.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -120,22 +120,6 @@ telescope.setup({
       },
     },
   },
-  extensions = { -- Your extension configuration goes here:
-    -- please take a look at the readme of the extension you want to configure
-    fzf = {
-      fuzzy = true, -- false will only do exact matching
-      override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case", -- or 'ignore_case' or 'respect_case'
-      -- the default case_mode is 'smart_case'
-    },
-    media_files = {
-      -- filetypes whitelist
-      -- defaults to {'png', 'jpg', 'mp4', 'webm', 'pdf'}
-      filetypes = { "png", "webp", "jpg", "jpeg" },
-      find_cmd = "rg", -- find command (defaults to `fd`)
-    },
-  },
 })
 
 -- Highlights

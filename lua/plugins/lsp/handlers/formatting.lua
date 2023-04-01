bf306a6170271c1bb1580a23800d2575bd60bbd5
daephx <daephxdev@gmail.com>
2023-04-01 07:19:34 -0700

    refactor: simplify lsp format filtering
    Rework autocmd to apply autocmd only the relevant buffers.
    
    Simply filtering logic to lean heavily on null-ls or fallback to
    language servers that provide formatting capabilities.
    
    Disable formatting autocmd for clients that have formatting capabilities
    disabled.
    
    Add toggle function so that formatting can be disabled.
    Add module level variable to handle format toggling.

--- lua/plugins/lsp/handlers/formatting.lua
+++ lua/plugins/lsp/handlers/formatting.lua
@@ -1,62 +1,43 @@
 local M = {}
 
-local disabled_formatters = {
-  global = { "copilot" },
-  filetypes = {},
-}
+M.autoformat = true
 
--- Determine if buffer has configured null-ls formatting generators for filetype
----@param bufnr integer
----@return boolean
-local function null_has_formatter(bufnr)
-  local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
-  local generators = require("null-ls.generators").get_available(
-    filetype,
-    require("null-ls.methods").internal.FORMATTING
-  )
-  return #generators > 0
+local notify = function(msg)
+  local level = vim.log.levels.INFO
+  vim.notify(msg, level, { title = "Format" })
 end
 
--- Get list of language servers attached to the current buffer
----@param bufnr integer
----@return table
-local function buf_get_clients(bufnr)
-  local list = {}
-  for _, client in pairs(vim.lsp.buf_get_clients(bufnr)) do
-    table.insert(list, client.name)
+-- Toggle document formatting on save
+M.toggle = function()
+  if vim.b.autoformat == false then
+    vim.b.autoformat = nil ---@type boolean|nil
+    M.autoformat = true
+  else
+    M.autoformat = not M.autoformat
   end
-  return list
+  if M.autoformat then
+    notify("[LSP]: Formatting on save 'Enabled'")
+  else
+    notify("[LSP]: Formatting on save 'Disabled'")
+  end
 end
 
 -- Activate document formatting filter to select a preferred formatter
----@param bufnr integer?
-M.format = function(bufnr)
-  bufnr = bufnr or 0
+M.format = function()
+  local bufnr = vim.api.nvim_get_current_buf() ---@type integer
+  if vim.b.autoformat == false then
+    return
+  end
+  local ft = vim.bo[bufnr].filetype ---@type string
+  local have_nls = #require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0
+
   vim.lsp.buf.format({
     bufnr = bufnr,
     filter = function(client)
-      local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
-
-      -- Filter user disable clients
-      local disabled_g = disabled_formatters.global or {}
-      if vim.tbl_contains(disabled_g, client.name) then
-        return false
+      if have_nls then
+        return client.name == "null-ls"
       end
-
-      -- Match disabled clients by filetype
-      local disabled_ft = disabled_formatters.filetypes[filetype] or {}
-      if vim.tbl_contains(disabled_ft, client.name) then
-        return false
-      end
-
-      -- Prefer null-ls or EFM if present in client list
-      if vim.tbl_contains(buf_get_clients(bufnr), "null-ls") then
-        if client.name ~= "null-ls" and null_has_formatter(bufnr) then
-          return false
-        end
-      end
-
-      return true
+      return client.name ~= "null-ls"
     end,
   })
 end
@@ -65,18 +46,32 @@ end
 ---@param client table
 ---@param bufnr integer
 M.enable_auto_formatting = function(client, bufnr)
-  local group = vim.api.nvim_create_augroup("LspFormatting", {})
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_create_user_command("Format", "do User LspFormatting", {})
-    vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+  -- Prevent formatting if client disabled it
+  local disabled = (
+    client.config
+    and client.config.capabilities
+    and client.config.capabilities.documentFormattingProvider == false
+  )
+  -- Apply autocmd if client supports formatting
+  if not disabled or client.supports_method("textDocument/formatting") then
     vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
       desc = "Apply Auto-formatting for to document on save",
-      group = group,
-      pattern = "*",
-      callback = function(ev)
-        M.format(ev.bufnr)
+      group = vim.api.nvim_create_augroup("LspFormat." .. bufnr, {}),
+      callback = function()
+        M.format()
       end,
     })
+
+    -- Add formatting user command
+    vim.api.nvim_create_user_command("Format", function()
+      M.format()
+    end, {})
+
+    -- Add toggle formatting user command
+    vim.api.nvim_create_user_command("FormatToggle", function()
+      M.toggle()
+    end, {})
   end
 end
 

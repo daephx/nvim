a142a7e386f3147274e2dfd834fa19df103c04f7
daephx <daephxdev@gmail.com>
2023-04-15 22:48:51 -0700

    fix: typo in undotree filetype options
    Option is `cursorlineopt`, not `cursorlineopts`.

--- after/ftplugin/undotree.lua
+++ after/ftplugin/undotree.lua
@@ -4,5 +4,5 @@ vim.opt_local.list = false
 vim.opt_local.spell = false
 vim.opt_local.number = false
 vim.opt_local.cursorline = true
-vim.opt_local.cursorlineopts = "both"
+vim.opt_local.cursorlineopt = "both"
 vim.opt_local.signcolumn = "no"

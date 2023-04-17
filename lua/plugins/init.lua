4c60dc1f91a320f627afcd0a9ebfcb39ddf87e98
daephx <daephxdev@gmail.com>
2023-04-17 02:41:43 -0700

    fix: lualine flash on startup: Lazyload `UIEnter`
    Setting lualine to load on the `VeryLazy` event caused a small flash on
    startup, this should allow it to load properly.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -62,7 +62,7 @@ require("lazy").setup({
   },
   { -- Blazing fast statusline
     "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
+    event = "UIEnter",
     config = config("plugins.lualine"),
     dependencies = {
       "nvim-tree/nvim-web-devicons",

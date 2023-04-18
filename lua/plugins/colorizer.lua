d0a00fed185557ed55d4d5bc59b671a41831e7da
daephx <daephxdev@gmail.com>
2023-04-17 23:20:56 -0700

    fix(plug): colorizer: update plugin options
    refactor: remove options that match plugin defaults
    
    fix: update buftype include/exclude rules
    
    - exclude nofile buftype instead of multiple plugin filetypes
    - exclude terminal buftype instead of just toggleterm
    
    fix: enabled all features for css filetypes
    
    This actually doesn't work without explicitly overriding any false
    values in the `user_default_options` table.

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -6,23 +6,19 @@ if not colorizer_ok then
 end
 
 colorizer.setup({
-  filetypes = {
+  buftypes = {
     "*",
-    "!lazy",
-    "!mason",
-    "!packer",
+    "!nofile",
     "!popup",
     "!prompt",
   },
+  filetypes = {
+    "*",
+    css = { css = true },
+  },
   user_default_options = {
-    RGB = true, -- #RGB hex codes
-    RRGGBB = true, -- #RRGGBB hex codes
+    RGB = false, -- #RGB hex codes
     names = false, -- "Name" codes like Blue
-    RRGGBBAA = true, -- #RRGGBBAA hex codes
-    rgb_fn = false, -- CSS rgb() and rgba() functions
-    hsl_fn = false, -- CSS hsl() and hsla() functions
-    css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
     mode = "background", -- Set the display mode.
   },
 })

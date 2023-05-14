a8c347e7da785b1f38161355d520b792c2b7f68e
daephx <daephxdev@gmail.com>
2023-05-14 05:32:48 -0700

    feat(toggleterm): add custom terminal layouts
    These terminals simply have custom settings for how they are displayed
    IE. a floating window at the bottom of the screen to mimic a horizontal
    split, similar to how telescope pickers are displayed. This can be
    helpful to prevent windows from being pushed around but comes with some
    caveats. Mostly this is in the form of buggy affects when focus is
    shifted as a new window is created.
    
    The bottom floating window can also be used within zenmode
    since it does not allow for splits to be created when zoomed in.
    
    Additionally there are other layouts such as fullscreen_float which
    basically makes it seem as if neovim is not even open and you're back in
    the main terminal. Or, the tab_float which take into account where the
    buffer and statusline are located to mimic how a new tab would look.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -50,6 +50,54 @@ local Terminal = require("toggleterm.terminal").Terminal
 
 local M = {}
 
+M.bottom_float = Terminal:new({
+  direction = "float",
+  float_opts = {
+    border = { "", "_", "", "", "", "", "", "" },
+    anchor = "SW",
+    width = vim.o.columns,
+    height = function(term)
+      local _, zenmode = pcall(function()
+        return require("zen-mode.view").is_open()
+      end)
+      local height = 20
+      local cmdheight = vim.o.cmdheight ~= 0 and vim.o.cmdheight or 0
+      local statusheight = vim.o.laststatus ~= 0 and 1 or 0
+      local botheight = zenmode == true and 0 or cmdheight + statusheight
+      term.float_opts.row = vim.o.lines - 1 - height - botheight
+      return height
+    end,
+  },
+})
+
+M.tab_float = Terminal:new({
+  direction = "float",
+  float_opts = {
+    border = false,
+    width = vim.o.columns,
+    height = function(term)
+      local cmdheight = vim.o.cmdheight
+      local tabline = vim.o.showtabline ~= 0 and 1 or 0
+      if vim.o.showtabline == 1 and vim.fn.tabpagenr("$") < 1 then
+        tabline = 0
+      end
+      local statusline = vim.o.laststatus ~= 0 and 1 or 0
+      local height = vim.o.lines - cmdheight - tabline - statusline
+      term.float_opts.row = 1 + tabline - 2
+      return height
+    end,
+  },
+})
+
+M.fullscreen_float = Terminal:new({
+  direction = "float",
+  float_opts = {
+    border = false,
+    width = vim.o.columns,
+    height = vim.o.lines,
+  },
+})
+
 local applications = {
   "btop",
   "htop",

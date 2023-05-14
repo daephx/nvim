a54d389ca7befef07f69836bb7529a2898ea2f86
daephx <daephxdev@gmail.com>
2023-05-14 04:03:23 -0700

    feat(zenmode): make zenmode more context aware
    refactor: define on_open outside of setup
    
    feat: add buftype and filetype exclusion rules
    
    This simply makes zenmode act as a fullscreen zoom window instead of
    restricting the size to the current textwidth value.
    
    feat: dynamically resize zenmode
    
    Takes into account the size of the current buffers textwidth, the size
    of the gutter and number line and the spacing of the colorcolumn to
    dynamically set the width of zenmode.

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -5,15 +5,36 @@ if not zenmode_ok then
   return
 end
 
+local filetypes = {
+  "fugitive",
+  "log",
+}
+local buftypes = {
+  "nofile",
+  "prompt",
+  "terminal",
+}
+
+local on_open = function(win)
+  -- Make certain buffers use all available columns
+  local bufnr = vim.api.nvim_win_get_buf(win)
+  local bt = vim.api.nvim_buf_get_option(bufnr, "buftype")
+  local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+  if vim.tbl_contains(buftypes, bt) or vim.tbl_contains(filetypes, ft) then
+    vim.api.nvim_win_set_width(win, vim.o.columns)
+  else
+    -- Calculate size based on gutter, textwidth and colorcolumn spacing
+    local ccol = tonumber(vim.o.colorcolumn:sub(2)) or 0
+    local textoff = vim.fn.getwininfo(win)[1].textoff
+    local width = vim.o.textwidth + textoff + ccol
+    vim.api.nvim_win_set_width(win, width)
+  end
+end
+
 zenmode.setup({
   backdrop = 1, -- Backdrop shade: Set to 1 to keep the same as Normal.
   -- callback where you can add custom code when the Zen window opens
-  on_open = function(win)
-    local bufnr = vim.api.nvim_win_get_buf(win)
-    if vim.api.nvim_buf_get_option(bufnr, "buftype") == "terminal" then
-      vim.api.nvim_win_set_width(win, 1000)
-    end
-  end,
+  on_open = on_open,
 })
 
 --- Keymaps ---

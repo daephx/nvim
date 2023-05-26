4e5fd977b309890d04255e5e58d3a0b0f33dd561
daephx <daephxdev@gmail.com>
2023-05-25 19:17:54 -0700

    fix(auto-session): improve session save/delete notify
    Default logging for plugin events are rather verbose and can be annoying
    if the relevant path string is too long, resulting in the need to "Press
    ENTER to continue" prompts.
    
    This sets logging back to `error` to suppress default logging output to
    the echo area. Following this change includes a basic wrapper that
    injects a custom output message for `SessionSave` and `SessionDelete`
    user commands.

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,6 +1,18 @@
 -- auto-session | A small automated session manager for Neovim
 -- https://github.com/rmagatti/auto-session
 
+local cmd_wrapper = function(cmd)
+  return function()
+    local action = (cmd):gsub("Session", "")
+    local cwd = vim.fn.getcwd()
+    vim.notify(("%s session: %s"):format(action, cwd), 2, {})
+    vim.cmd(cmd)
+  end
+end
+
+local delete_session = cmd_wrapper("SessionDelete")
+local save_session = cmd_wrapper("SessionSave")
+
 return {
   "rmagatti/auto-session",
   event = "VimEnter",
@@ -13,12 +25,12 @@ return {
     },
   },
   keys = {
-    { "<leader>sd", "<cmd>SessionDelete<CR>", desc = "Delete current session" },
-    { "<leader>ss", "<cmd>SessionSave<CR>", desc = "Save current session" },
+    { "<leader>sd", delete_session, desc = "Delete current session" },
+    { "<leader>ss", save_session, desc = "Save current session" },
     { "<leader>sl", "<cmd>Telescope session-lens search_session<CR>", desc = "Show Session list" },
   },
   opts = {
-    log_level = "info",
+    log_level = "error",
     auto_restore_enabled = true,
     auto_save_enabled = true,
     auto_session_suppress_dirs = { "/", "~/", "~/Documents", "~/Downloads", "~/Projects" },

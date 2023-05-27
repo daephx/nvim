3b13a84933e6f1da07f96fa1ba1f8e9201a44c8d
daephx <daephxdev@gmail.com>
2023-05-26 19:03:48 -0700

    feat(toggleterm): expose custom terminals from user opts
    Define terminal options as tables from within `opts.terminals`. These
    tables will be converted into custom terminals and be accessible from
    the plugins modules return table using the following command:
    `require('plugins.toggleterm').terminals`.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,8 @@
 -- toggleterm.nvim | easily manage multiple terminal windows
 -- https://github.com/akinsho/toggleterm.nvim
 
+local terminals = {}
+
 -- Callback to determine the size of terminal windows
 local function size(term)
   if term.direction == "horizontal" then
@@ -24,67 +26,15 @@ local function on_close()
   end
 end
 
---- Terminals ---
-
--- Export custom terminal settings for different use cases
-
--- local Terminal = require("toggleterm.terminal").Terminal
-
--- local M = {}
-
--- M.bottom_float = Terminal:new({
---   direction = "float",
---   float_opts = {
---     border = { "", "_", "", "", "", "", "", "" },
---     anchor = "SW",
---     width = vim.o.columns,
---     height = function(term)
---       local _, zenmode = pcall(function()
---         return require("zen-mode.view").is_open()
---       end)
---       local height = 20
---       local cmdheight = vim.o.cmdheight ~= 0 and vim.o.cmdheight or 0
---       local statusheight = vim.o.laststatus ~= 0 and 1 or 0
---       local botheight = zenmode == true and 0 or cmdheight + statusheight
---       term.float_opts.row = vim.o.lines - 1 - height - botheight
---       return height
---     end,
---   },
--- })
-
--- M.tab_float = Terminal:new({
---   direction = "float",
---   float_opts = {
---     border = false,
---     width = vim.o.columns,
---     height = function(term)
---       local cmdheight = vim.o.cmdheight
---       local tabline = vim.o.showtabline ~= 0 and 1 or 0
---       if vim.o.showtabline == 1 and vim.fn.tabpagenr("$") < 1 then
---         tabline = 0
---       end
---       local statusline = vim.o.laststatus ~= 0 and 1 or 0
---       local height = vim.o.lines - cmdheight - tabline - statusline
---       term.float_opts.row = 1 + tabline - 2
---       return height
---     end,
---   },
--- })
-
--- M.fullscreen_float = Terminal:new({
---   direction = "float",
---   float_opts = {
---     border = false,
---     width = vim.o.columns,
---     height = vim.o.lines,
---   },
--- })
-
 -- Generate custom terminals from user opts
 ---@param opts table
 local initialize_terminals = function(opts)
-  local terminals = {}
   local Terminal = require("toggleterm.terminal").Terminal
+  -- Process manually defined terminals
+  for name, term in pairs(opts.terminals) do
+    terminals[name] = Terminal:new(term)
+  end
+  -- Process basic application terminals
   for _, app in pairs(opts.applications) do
     if vim.fn.executable(app) == 1 then
       local command = app:gsub("^%l", string.upper)
@@ -104,6 +54,8 @@ return {
     { "<C-\\>" },
     { "<leader>tt", "<cmd>ToggleTerm direction=tab<CR>", desc = "Terminal (tab)" },
   },
+  -- Terminals exposed in spec for easy access
+  terminals = terminals,
   config = function(_, opts)
     require("toggleterm").setup(opts)
     initialize_terminals(opts)
@@ -125,6 +77,54 @@ return {
       "htop",
       "lazydocker",
       "lazygit",
+    },
+    -- Manually define terminals for more fine tuning
+    terminals = {
+      fullscreen_float = {
+        direction = "float",
+        float_opts = {
+          border = false,
+          width = vim.o.columns,
+          height = vim.o.lines,
+        },
+      },
+      tab_float = {
+        direction = "float",
+        float_opts = {
+          border = false,
+          width = vim.o.columns,
+          height = function(term)
+            local cmdheight = vim.o.cmdheight
+            local tabline = vim.o.showtabline ~= 0 and 1 or 0
+            if vim.o.showtabline == 1 and vim.fn.tabpagenr("$") < 1 then
+              tabline = 0
+            end
+            local statusline = vim.o.laststatus ~= 0 and 1 or 0
+            local height = vim.o.lines - cmdheight - tabline - statusline
+            term.float_opts.row = 1 + tabline - 2
+            return height
+          end,
+        },
+      },
+      bottom_float = {
+        direction = "float",
+        float_opts = {
+          border = { "", "_", "", "", "", "", "", "" },
+          anchor = "SW",
+          width = vim.o.columns,
+          height = function(term)
+            local _, zenmode = pcall(function()
+              return require("zen-mode.view").is_open()
+            end)
+            local height = 20
+            local cmdheight = vim.o.cmdheight ~= 0 and vim.o.cmdheight or 0
+            local statusheight = vim.o.laststatus ~= 0 and 1 or 0
+            local botheight = zenmode == true and 0 or cmdheight + statusheight
+            term.float_opts.row = vim.o.lines - 1 - height - botheight
+            return height
+          end,
+        },
+      },
     },
   },
 }

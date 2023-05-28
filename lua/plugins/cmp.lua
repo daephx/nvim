519f37a9bbf95b2d905f05826b97b56ce4d20a57
daephx <daephxdev@gmail.com>
2023-05-28 04:37:37 -0700

    fix(cmp): disable insert on select behaviour
    This prevents the completion item from being partially inserted when
    selecting from the completion menu. Instead the item will only be
    displayed as ghost text until the user confirms their selection.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -131,8 +131,8 @@ return {
         ["<C-Down>"] = cmp.mapping.scroll_docs(1),
         ["<C-u>"] = cmp.mapping.scroll_docs(-4),
         ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = "none" }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = "none" }),
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         ["<C-e>"] = cmp.mapping({
           i = cmp.mapping.abort(),

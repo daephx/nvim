3e14a04edba347c1c046596877e94e4882bfb152
daephx <daephxdev@gmail.com>
2023-06-16 12:45:59 -0700

    refactor(opts): organize options into general groups
    Cleanup comments and loosely organize options.
    
    Many of these settings don't fit cleanly into logical categories. But
    for the most part, many options play off each other and are often
    changed in tandem. So, it makes since that they should be defined in the
    same section.

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,9 +6,12 @@
 -- ====/_/==============================
 -- Neovim general editor options/settings
 
-vim.g.mapleader = " " -- Default leader for keymaps
-vim.g.localleader = "\\" -- Default localleader for keymaps
 vim.g.border = "single" -- Default border for floating windows
+vim.g.localleader = "\\" -- Default localleader for keymaps
+vim.g.mapleader = " " -- Default leader for keymaps
+
+vim.opt.background = "dark" -- Adjusts the default color groups for background type
+vim.opt.termguicolors = true -- Enable true color support
 
 vim.opt.textwidth = 80 -- Limit length of characters per line
 vim.opt.tabstop = 4 -- Number of spaces to represent tab characters
@@ -16,43 +19,44 @@ vim.opt.softtabstop = 4 -- Number of spaces for tabs during edit operations
 vim.opt.shiftwidth = 4 -- Number of spaces for expandtab indent
 vim.opt.expandtab = true -- Convert tabs characters to spaces
 vim.opt.shiftround = true -- Round indent to shift width
-vim.opt.autoindent = false -- Auto apply indentation
-vim.opt.autoread = true -- Load changes made outside of Vim
-vim.opt.linebreak = true -- Split on line break
-vim.opt.colorcolumn = "+1" -- Visually display character line limit
-vim.opt.background = "dark" -- Adjusts the default color groups for background type
-vim.opt.clipboard = { "unnamed", "unnamedplus" } -- Configure system clipboard support
-vim.opt.cmdwinheight = 5 -- Set size of command history buffer
-vim.opt.wrap = false -- Wrap text when line is too long
+vim.opt.autoindent = false -- Auto apply indentation on newline
+
+vim.opt.number = true -- Display absolute line number in gutter
+vim.opt.relativenumber = true -- Display relative line number in gutter
 vim.opt.cursorline = true -- Highlight the text line of the cursor
 vim.opt.cursorlineopt = "number" -- Control how 'cursorline' is displayed
-vim.opt.laststatus = 3 -- Apply statusline options
-vim.opt.lazyredraw = true -- Execute macros faster
-vim.opt.mouse = "a" -- Enable mouse support
-vim.opt.showmode = false -- Manage whether EOL message is shown
-vim.opt.pumblend = 24 -- Pop-up menu transparency
-vim.opt.pumheight = 12 -- Pop-up menu height
-vim.opt.pumwidth = 16 -- Pop-up menu thickness
-vim.opt.scrolloff = 8 -- Keep cursor closer to center vertically
-vim.opt.showtabline = 2 -- Always show tabline
-vim.opt.sidescrolloff = 2 -- Keep cursor closer to center horizontally
+vim.opt.colorcolumn = "+1" -- Visually display character line limit
 vim.opt.signcolumn = "yes:2" -- Sign gutter options: yes|no : int = size
+vim.opt.scrolloff = 8 -- Keep cursor closer to center vertically
+vim.opt.sidescrolloff = 2 -- Keep cursor closer to center horizontally
+vim.opt.laststatus = 3 -- Apply statusline options
+vim.opt.showtabline = 2 -- Always show tabline
+vim.opt.cmdwinheight = 5 -- Set size of command history buffer
+vim.opt.winminwidth = 5 -- The minimal width for windows
+
+vim.opt.mouse = "a" -- Enable mouse support
+vim.opt.wrap = false -- Wrap text when line is too long
+vim.opt.linebreak = true -- Split on line break
 vim.opt.ignorecase = true -- Case insensitive search
 vim.opt.smartcase = true -- Override search if pattern contains uppercase
+
+vim.opt.autoread = true -- Load changes made outside of Vim
+vim.opt.lazyredraw = true -- Execute macros faster
+vim.opt.showmode = false -- Manage whether EOL message is shown
 vim.opt.splitbelow = true -- Open new windows below current
 vim.opt.splitright = true -- Open new windows right of current
-vim.opt.termguicolors = true -- Enable true color support
+
 vim.opt.timeoutlen = 500 -- Timeout for mapped sequence
 vim.opt.ttimeoutlen = 0 -- Timeout for key code sequence
 vim.opt.updatetime = 50 -- Timeout idle before CursorHold or updating swapfile
-vim.opt.number = true -- Display absolute line number in gutter
-vim.opt.relativenumber = true -- Display relative line number in gutter
-vim.opt.swapfile = false -- Disable swap files
+vim.opt.swapfile = false -- Use a swapfile for the current buffer
 vim.opt.writebackup = false -- Backup before overwriting a file
 vim.opt.undofile = true -- Cache persistent undo
 
-vim.opt.shortmess:append("cfI") -- Avoid 'hit-enter' prompts
-vim.opt.whichwrap:append("<,>,[,]") -- Allows navigation wrapping to previous/next lines
+-- Popup menu options
+vim.opt.pumblend = 24 -- Pop-up menu transparency
+vim.opt.pumheight = 12 -- Pop-up menu height
+vim.opt.pumwidth = 16 -- Pop-up menu thickness
 
 -- Control vim Spell checking
 vim.opt.spell = true
@@ -61,6 +65,12 @@ vim.opt.spelloptions = { "camel", "noplainbuffer" }
 
 -- Insert mode completion
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- Configure system clipboard support
+vim.opt.clipboard = { "unnamed", "unnamedplus" }
+
+vim.opt.whichwrap:append("<,>,[,]") -- Allows navigation wrapping to previous/next lines
+vim.opt.shortmess:append("cfI") -- Avoid 'hit-enter' prompts
 
 -- Describes how auto-format is handled
 -- See `:h fo-table` for details and more options
@@ -99,16 +109,6 @@ vim.opt.diffopt = {
   "vertical", -- Split diff vertically
 }
 
--- Define preferred font family for GUI clients
-vim.opt.guifont = {
-  "FiraCode NFM Retina:h12",
-  "FiraCode NFM:h12",
-  "CaskaydiaCove Nerd Font:h12",
-  "CaskaydiaCove NF:h12",
-}
-
---- Folding ---
-
 -- Define how folded lines should be displayed
 ---@return string
 function _G._foldtext()
@@ -133,8 +133,7 @@ vim.opt.foldmethod = "expr" -- use treesitter for folding
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldtext = "v:lua._foldtext()"
 
---- Wildmenu ---
-
+-- Wildmenu options
 vim.opt.wildmenu = true -- Command-line completion
 vim.opt.wildignorecase = true -- Case insensitive command-line completion
 vim.opt.wildmode = "full:lastused" -- Command-line completion mode
@@ -155,8 +154,6 @@ vim.opt.wildignore = {
   "*.pyc", -- Python byte code
   "*_build/*", -- Private build artifacts
 }
-
--- Virtual Characters ---
 
 -- Virtual strings to use in 'list' mode
 vim.opt.list = true -- Enable listchars
@@ -197,8 +194,6 @@ vim.opt.fillchars:append({
   vertright = "┣",
 })
 
---- Colors ---
-
 -- Global highlight overrides
 vim.g.colors = {
   LspInfoBorder = { link = "FloatBorder" },
@@ -206,14 +201,19 @@ vim.g.colors = {
   NormalFloat = { link = "Normal" },
 }
 
---- Netrw ---
+-- Define preferred font family for GUI clients
+vim.opt.guifont = {
+  "FiraCode NFM Retina:h12",
+  "FiraCode NFM:h12",
+  "CaskaydiaCove Nerd Font:h12",
+  "CaskaydiaCove NF:h12",
+}
 
+-- Netrw specific options
 vim.g.netrw_banner = 0
 vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
 vim.g.netrw_liststyle = 3 -- Default to tree view
 vim.g.netrw_localrmdir = "rm -r"
-
---- Neovide ---
 
 -- Options that only apply to neovide GUI
 if vim.g.is_neovide == 1 then
@@ -225,11 +225,8 @@ if vim.g.is_neovide == 1 then
   vim.g.neovide_remember_window_size = true
 end
 
---- Windows ---
-
--- Options that only apply to nt/windows
+-- Set default shell to PowerShell in nt/windows
 if vim.fn.has("win32") == 1 then
-  -- Set default shell to PowerShell
   vim.opt.shell = vim.fn.executable("pwsh") and "pwsh" or "powershell"
   vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
   vim.opt.shellredir = "| Out-File -Encoding UTF8"

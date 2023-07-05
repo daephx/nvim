28221cd2c0b4dde38dfd81e946e5172c772158c1
daephx <daephxdev@gmail.com>
2023-07-05 01:53:34 -0700

    refactor(lualine): improve functionality of lsp_client component
    docs(lualine): add copyright to head of lsp_client component
    
    refactor(lualine): change func order in lsp_client component
    
    refactor(lualine): replace deprecated func `buf_get_clients`
    
    Now uses function `get_active_clients` with the buffer/bufnr set to 0 to
    limit the client list to only the active buffer.
    
    refactor(lualine): simplify icon settings for lsp_client component
    
    Don't assume the position or color of the icon and let those settings be
    handled by the user.
    
    Notably, the color values did not seem to have an effect with the
    previous default configuration.
    
    docs(lualine): add luadoc to helpers in lsp_client component
    
    fix(lualine): remove between pyright and venv labels
    
    fix(lualine): use list instead of tbl for lsp_client component
    
    Table functions can have issues where numerical keys for the given
    client names can be added in weird ways, making basic list operation not
    function properly and resulting in duplicate or missing entries.
    
    This also inverts the list_extends arguments for clients in the
    update_status function, allowing client names to appear as the last
    entry within the client list without explicit appending after null-ls
    sources have been determined.
    
    feat(lualine): allow configurable separator for lsp_client entries
    
    Client names are separated with a specific character surrounded by
    spaces, which is now set within the default_options table to allow the
    user to overwrite it.
    
    refactor(lualine): wrap null_sources helper logic in pcall
    
    Improves how null-ls sources are obtained by wrapping all logic within a
    pcall to remove the return early guard clause, and also uses a general
    for-loop instead of `vim.tbl_map`, allowing the removal of duplicate
    sources with the same name, such as pythons `ruff:
    diagnostic/formatting` sources.
    
    refactor(lualine): rework update_status return string logic
    
    - Remove local variable: `next` - use directly
    - Invert logic to return empty of list has no entries.
    
    fix(lualine): add missing return in lsp_client `update_status`

--- lua/lualine/components/lsp_client.lua
+++ lua/lualine/components/lsp_client.lua
@@ -1,4 +1,42 @@
+-- Copyright (c) 2021-2023 daephx
+-- MIT license, see LICENSE for more details.
 local lsp_clients = require("lualine.component"):extend()
+
+local default_options = {
+  condition = function()
+    local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+    return #clients > 0
+  end,
+  separator = "",
+  icon = { "" },
+  exclude_sources = { "gitsigns" },
+}
+
+function lsp_clients:init(options)
+  lsp_clients.super.init(self, options)
+  options.colors = self.extend(self, "colors")
+  self.options = vim.tbl_deep_extend("force", default_options, self.options or {})
+end
+
+-- Return filtered list of buffer attached null-ls source names
+---@param exclude table
+---@return table
+local null_sources = function(exclude)
+  local _, sources = pcall(function()
+    local ret = {}
+    local sources = require("null-ls.sources")
+    local available = sources.get_available(vim.bo.filetype)
+    for _, client in pairs(available) do
+      local is_exclude = vim.list_contains(exclude, client.name)
+      local is_present = vim.list_contains(ret, client.name)
+      if not is_exclude and not is_present then
+        table.insert(ret, client.name)
+      end
+    end
+    return ret
+  end)
+  return sources
+end
 
 -- Modify client name for style or to add extra metadata
 ---@param client table
@@ -8,60 +46,30 @@ local format_client_name = function(client)
     -- Check if python lsp and has virtual environment
     if client.config.settings.python["venv_name"] ~= nil then
       local venv_name = client.config.settings.python.venv_name
-      return string.format("%s (%s)", client.name, venv_name)
+      return string.format("%s(%s)", client.name, venv_name)
     end
   end
   return client.name
 end
 
-local default_options = {
-  condition = function()
-    return #vim.lsp.buf_get_clients() > 0
-  end,
-  icon = {
-    "",
-    align = "right",
-    color = { fg = "Gray", gui = "bold" },
-  },
-  -- Exclude certain sources from being displayed
-  exclude_sources = {
-    "gitsigns",
-  },
-}
-
-function lsp_clients:init(options)
-  lsp_clients.super.init(self, options)
-  options.colors = self.extend(self, "colors")
-  self.options = vim.tbl_deep_extend("force", default_options, self.options or {})
-end
-
--- Return table of null-ls enabled sources
-local null_sources = function(exclude)
-  local sources = require("null-ls.sources")
-  return vim.tbl_map(function(source)
-    if not vim.tbl_contains(exclude, source.name) then
-      return source.name
-    end
-  end, sources.get_available(vim.bo.filetype))
-end
-
 function lsp_clients:update_status()
-  local next = next
   local clients = {}
-  local buf_clients = vim.lsp.buf_get_clients()
+  local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
   local exclude = self.options.exclude_sources
   for _, client in pairs(buf_clients) do
     if client.name == "null-ls" then
-      clients = vim.tbl_extend("keep", clients, null_sources(exclude))
-    elseif not vim.tbl_contains(exclude, client.name) then
+      clients = vim.list_extend(null_sources(exclude), clients)
+    elseif not vim.list_contains(exclude, client.name) then
       table.insert(clients, format_client_name(client))
     end
   end
-  -- If list is empty return empty string
-  if next(clients) == nil then
+
+  if next(clients) ~= nil then
+    local sep = (" %s "):format(default_options.separator)
+    return table.concat(clients, sep)
+  else
     return ""
   end
-  return table.concat(clients, "  ")
 end
 
 return lsp_clients

b4a028013cecfa89f5dd134e3f9931bd9056745c
daephx <daephxdev@gmail.com>
2023-07-25 03:59:39 -0700

    refactor(lsp): move group outside func for lsp reference hl

--- lua/plugins/lsp/handlers/highlight.lua
+++ lua/plugins/lsp/handlers/highlight.lua
@@ -1,11 +1,12 @@
 local M = {}
 
+local augroup = vim.api.nvim_create_augroup("LspDocumentHighlight", { clear = true })
+
 -- Highlight references under cursor
 ---@param client table
 ---@param bufnr integer
 M.enable_document_highlighting = function(client, bufnr)
   if client.supports_method("textDocument/documentHighlight") then
-    local augroup = vim.api.nvim_create_augroup("LspDocumentHighlight", { clear = true })
     vim.api.nvim_create_autocmd({ "CursorHold" }, {
       buffer = bufnr,
       desc = "Apply LSP document reference highlighting on CursorHold",

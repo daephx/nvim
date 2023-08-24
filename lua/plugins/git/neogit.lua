fde7c4fe79f95c662fe84d585970df7f8bd19995
daephx <daephxdev@gmail.com>
2023-08-24 05:42:59 -0700

    fix(neogit): update config inline w/ current revision
    - feat: add scrolloff option autocmd to prevent status buffer scrolling
    
    - fix: autocmd pattern not registering wildcard, use explicit filetypes
    - fix: remove config.sections have been renamed and the defaults are good now
    - fix: remove option `use_magit_keybindings`, no longer available upstream
    
    - refactor: move  autocmds to the lazy.spec:init method
    - refactor: remove custom highlights, use default or colorscheme options
    - refactor: remove fillchars replace eob with whitespace, the tilde ain't so bad
    - refactor: use separate augroup/s for give each autocmd

--- lua/plugins/git/neogit.lua
+++ lua/plugins/git/neogit.lua
@@ -16,70 +16,38 @@ return {
   opts = {
     kind = "tab",
     disable_context_highlighting = true,
-    use_magit_keybindings = true,
     integrations = {
       diffview = true,
       telescope = true,
     },
-    sections = {
-      staged = {
-        folded = false,
-      },
-      unstaged = {
-        folded = true,
-      },
-      untracked = {
-        folded = true,
-      },
-      unmerged = {
-        folded = false,
-      },
-      unpulled = {
-        folded = true,
-      },
-      stashes = {
-        folded = true,
-      },
-      recent = {
-        folded = false,
-      },
-    },
   },
-  config = function(_, opts)
-    local neogit = require("neogit")
-    neogit.setup(opts)
-
-    --- Highlights ---
-
-    local hl = vim.api.nvim_set_hl
-
-    hl(0, "NeogitCommitViewHeader", { link = "Constant" })
-    hl(0, "NeogitDiffContextHighlight", { link = "ColorColumn" })
-    hl(0, "NeogitHunkHeader", { bg = "none", fg = "#D670D6" })
-    hl(0, "NeogitObjectId", { bg = "none", fg = "#A056BB" })
-
-    --- Autocmds ---
-
-    local group = vim.api.nvim_create_augroup("NeogitHooks", {})
-
+  init = function()
     vim.api.nvim_create_autocmd("FileType", {
       desc = "Local options to apply to neogit filetypes",
-      group = group,
+      group = vim.api.nvim_create_augroup("NeogitBufferOptions", {}),
       pattern = "Neogit*",
       callback = function()
-        vim.opt_local.spell = false
-        vim.opt_local.number = false
-        vim.opt_local.relativenumber = false
         vim.opt_local.colorcolumn = "0"
-        vim.opt_local.fillchars = { eob = " " }
+        vim.opt_local.spell = false
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      desc = "Prevent status buffers from over-scrolling",
+      group = vim.api.nvim_create_augroup("NeogitStatusOptions", {}),
+      pattern = { "NeogitPopup", "NeogitStatus" },
+      callback = function()
+        vim.opt_local.scrolloff = 999
       end,
     })
 
     vim.api.nvim_create_autocmd("User", {
       desc = "Close Neogit after pushing to remote repository",
-      group = group,
+      group = vim.api.nvim_create_augroup("NeogitPushOptions", {}),
       pattern = "NeogitPushComplete",
-      callback = neogit.close,
+      callback = function()
+        require("neogit").close()
+      end,
     })
   end,
 }

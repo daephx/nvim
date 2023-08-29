e3971cc1a0a59b5346733595b36eacb2779c21ac
daephx <daephxdev@gmail.com>
2023-08-29 06:31:30 -0700

    refactor(lsp): cleanup unnecessary handler definitions
    Much of these where commented out and not being utilized.

--- lua/plugins/lsp/handlers/init.lua
+++ lua/plugins/lsp/handlers/init.lua
@@ -11,14 +11,5 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = vim.g.border,
 })
 
--- vim.lsp.handlers['textDocument/rename'] = require('plugins.lsp.handlers.rename').incremental_rename
--- vim.lsp.handlers['textDocument/rename'] = vim.lsp.buf.rename.float
-
-vim.lsp.handlers["textDocument/definition"] =
-  require("plugins.lsp.handlers.definition").goto_definition()
--- vim.lsp.handlers['textDocument/documentSymbol'] = utils.lsp_symbols_handler()
--- vim.lsp.handlers['textDocument/implementation'] = utils.location_handler('LSP Implementations', 'implementations')
--- vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {})
--- vim.lsp.handlers['textDocument/references'] = utils.location_handler()
--- vim.lsp.handlers['textDocument/typeDefinition'] = utils.location_handler('LSP Type Definitions', 'typeDefinitions')
--- vim.lsp.handlers['workspace/symbol'] = utils.lsp_symbols_handler()
+local definition = require("plugins.lsp.handlers.definition")
+vim.lsp.handlers["textDocument/definition"] = definition.goto_definition

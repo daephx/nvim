10ac3b58101187a01072da72bad490f1dda6d141
daephx <daephxdev@gmail.com>
2023-08-29 03:26:24 -0700

    feat(lsp): add basic config for taplo-lsp

---
+++ lua/plugins/lsp/settings/taplo.lua
@@ -0,0 +1,22 @@
+-- taplo: A TOML toolkit written in Rust
+-- https://github.com/tamasfe/taplo
+
+return {
+  settings = {
+    evenBetterToml = {
+      schema = {
+        enabled = true,
+        repositoryEnabled = true,
+        repositoryUrl = "https://taplo.tamasfe.dev/schema_index.json",
+      },
+      cachePath = vim.fn.stdpath("cache") .. "/taplo",
+      formatter = {
+        alignEntries = false,
+        allowedBlankLines = 1,
+        indentEntries = false,
+        indentTables = false,
+        reorderKeys = false,
+      },
+    },
+  },
+}

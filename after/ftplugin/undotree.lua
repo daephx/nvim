f1bd7ab2ba4be6bbbbf35e1efdc1a922d2a84264
daephx <daephxdev@gmail.com>
2023-09-11 10:01:09 -0700

    refactor(undotree): move filetype options to plugin autocmd

--- after/ftplugin/undotree.lua
+++
@@ -1,8 +0,0 @@
--- after/ftplugin/undotree
-
-vim.opt_local.list = false
-vim.opt_local.spell = false
-vim.opt_local.number = false
-vim.opt_local.cursorline = true
-vim.opt_local.cursorlineopt = "both"
-vim.opt_local.signcolumn = "no"
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -7,4 +7,19 @@ return {
   keys = {
     { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Undotree" },
   },
+  init = function()
+    vim.api.nvim_create_autocmd({ "FileType" }, {
+      desc = "Set buffer options for undotree filetype",
+      group = vim.api.nvim_create_augroup("UndotreeOptions", {}),
+      pattern = { "undotree" },
+      callback = function()
+        vim.opt_local.list = false
+        vim.opt_local.spell = false
+        vim.opt_local.number = false
+        vim.opt_local.cursorline = true
+        vim.opt_local.cursorlineopt = "both"
+        vim.opt_local.signcolumn = "no"
+      end,
+    })
+  end,
 }

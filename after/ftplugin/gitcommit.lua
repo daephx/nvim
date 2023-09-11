6d2cc493021af2df5c3107c2807a2743dcb3d49b
daephx <daephxdev@gmail.com>
2023-09-11 09:53:10 -0700

    fix(ftplugin): override comment string for gitcommit filetypes

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -11,6 +11,7 @@ vim.opt_local.signcolumn = "yes:1"
 vim.opt_local.textwidth = 72
 vim.opt_local.colorcolumn = "+1"
 vim.opt_local.iskeyword:append("-")
+vim.opt_local.commentstring = "#%s"
 
 vim.opt_local.formatoptions = {
   c = false, -- Auto-wrap comments using 'textwidth'

b5ff8a354c1092bea99ac520a247a01ecc9ccb7b
daephx <daephxdev@gmail.com>
2023-10-18 02:25:57 -0700

    fix: disable cursorline in terminal buffers

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -14,6 +14,7 @@ local set_terminal_options = function()
   vim.opt_local.scrolloff = 0
   vim.opt_local.sidescrolloff = 0
   vim.opt_local.signcolumn = "no"
+  vim.opt_local.cursorline = false
   vim.opt_local.winfixheight = true
   vim.opt_local.fillchars = { eob = " " }
 end

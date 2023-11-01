9041af1fb943ad11939cc4ea32b3d254b5422a7c
daephx <daephxdev@gmail.com>
2023-11-01 08:23:06 -0700

    refactor: improve module/reference `config.colors`
    Reworks the colorscheme helper module to have better naming for
    functions, improve function definitions, fix minor bugs.
    
    - docs: reword autocmd and luadoc descriptions
    - docs: use triple dashes for luadoc comments
    
    - fix: add nil check for filepath in load function
    - fix: call colorscheme load autocmd on `VimEnter`
    - fix: default overrides not loaded if colorscheme not set
    - fix: update terminal colors on colorscheme event
      Terminal color are not updated when changing the active colorscheme.
      By changing the variables `g:terminal_color_{0..15}` and restarting
      the terminal buffer, we can get the correct colors.
    
    - refactor: define colors module augroup as variable
    - refactor: improve get highlight function
    - refactor: rename functions to shorthand names: more like `vim.api`
    - refactor!: remove runtime check for plugins/colors
      prefer using a local autocmd or the plugins builtin override options
    
    - feat: add func to register colorscheme override autocmd
      As stated in the relevant luadoc: This function extends the set_hl
      function by wrapping it in an autocmd so that colorschemes that don't
      provide their own override capabilities can still be easily modified.

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -8,7 +8,7 @@
 
 local M = {}
 
--- Check if main color scheme file is available in the current runtimepath
+---Check if colorscheme filename is available in the runtimepath
 ---@param colors_name string
 ---@return boolean
 M.is_available = function(colors_name)
@@ -21,18 +21,16 @@ M.is_available = function(colors_name)
   return false
 end
 
--- Get color table from highlight group with optional section: fg, bg
----@param group string
----@param hl string|nil
+---Get color table from highlight group
+---@param name string higroup name
 ---@return table
-M.get_highlights = function(group, hl)
-  local labels = { background = "bg", foreground = "fg", special = "sp" }
-  local tbl = vim.api.nvim_get_hl_by_name(group, hl or "")
+M.get_hl = function(name)
+  local labels = { "bg", "fg", "sp" }
+  local colors = vim.api.nvim_get_hl(0, { name = name })
   local res = {}
-  for k, v in pairs(tbl) do
-    local label = labels[k]
-    if label ~= nil then
-      res[label] = string.format("#%06x", v)
+  for k, v in pairs(colors) do
+    if vim.list_contains(labels, k) then
+      res[k] = string.format("#%06x", v)
     else
       res[k] = v
     end
@@ -40,30 +38,45 @@ M.get_highlights = function(group, hl)
   return res
 end
 
--- Extend table for highlight definitions for nvim_set_hl
+---Extend table for highlight definitions for nvim_set_hl
 ---@param group string
 ---@param tbl table
 ---@return table
-M.extend_highlights = function(group, tbl)
+M.extend_hl = function(group, tbl)
   local colors = M.get_highlights(group)
   return vim.tbl_extend("force", colors, tbl)
 end
 
--- Wrapper for nvim_set_hl that applies higroups from table definitions
----@param opts function|table
-M.set_highlights = function(opts)
-  if type(opts) == "function" then
-    opts = opts() ---@type table
+---Wrapper for nvim_set_hl that applies higroups from table definitions
+---@param colors table|function
+M.set_hl = function(colors)
+  if type(colors) == "function" then
+    colors = colors() ---@type table
   end
-  for group, colors in pairs(opts) do
-    vim.api.nvim_set_hl(0, group, colors)
+  for group, hl in pairs(colors) do
+    vim.api.nvim_set_hl(0, group, hl)
   end
 end
 
+---extend set_hl to register an autocmd that will run for the provided colorscheme
+---@param name string Name of the colorscheme
+---@param colors table Table of highlight definitions
+M.set_hl_autocmd = function(name, colors)
+  local groups_name = ("ColorScheme_%s"):format(name)
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    desc = ("Override highlights for theme: %s"):format(name),
+    group = vim.api.nvim_create_augroup(groups_name, { clear = true }),
+    pattern = name,
+    callback = function()
+      M.set_hl(colors)
+    end,
+  })
+end
+
 ---Get filepath for matching colorscheme name in after/colors directory.
----@param name string # Name of the desired colorscheme
----@param rtp string # Parent directory to search for runtime files
----@return string|nil # Path string to first matching runtime file
+---@param name string Name of the desired colorscheme
+---@param rtp string Parent directory to search for runtime files
+---@return string|nil Path string to first matching runtime file
 local get_colors_file = function(name, rtp)
   local files = vim.api.nvim_get_runtime_file(rtp .. "/*", true)
   for _, path in pairs(files) do
@@ -74,43 +87,47 @@ local get_colors_file = function(name, rtp)
   end
 end
 
--- Runtime paths to check for colorscheme overrides
-local runtime = {
-  "lua/plugins/colors",
-  "after/colors",
-}
+---Load colorscheme from runtime path: after/colors
+---@param name string Name of the desired colorscheme
+M.load = function(name)
+  local filepath = get_colors_file(name, "after/colors")
 
----Load colorscheme from runtime path.
----@param name string # Name of the desired colorscheme
-local load = function(name)
-  for _, rtp in pairs(runtime) do
-    local filepath = get_colors_file(name, rtp) or ""
+  -- Match extension for loading vimscript files
+  if filepath and filepath:match(".+%.vim") then
+    vim.cmd.source(filepath)
+  end
 
-    -- Match extension for loading vimscript files
-    if filepath:match(".+%.vim") then
-      vim.cmd.source(filepath)
-    end
-
-    -- Safely load lua file for theme highlights
-    local theme_ok, theme = pcall(dofile, filepath)
-    if theme_ok and type(theme) == "table" then
-      M.set_highlights(theme["colors"] or {})
-    end
+  -- Safely load lua file for theme highlights
+  local theme_ok, theme = pcall(dofile, filepath)
+  if theme_ok and type(theme) == "table" then
+    M.set_hl(theme["colors"] or {})
   end
 
   -- Apply global highlights variable for all colorschemes
   if vim.g.colors then
-    M.set_highlights(vim.g.colors or {})
+    M.set_hl(vim.g.colors or {})
   end
 end
 
 --- Autocmds ---
 
-vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  desc = "Load/Reload color scheme module when colorscheme changes",
-  group = vim.api.nvim_create_augroup("ColorSchemeOverrides", {}),
+local group = vim.api.nvim_create_augroup("ColorSchemeFix", {})
+
+vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+  desc = "Automatically load/reload colorscheme changes",
+  group = group,
   callback = function(ev)
-    load(ev.match)
+    M.load(vim.g.colors_name and ev.match or "default")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "ColorSchemePre" }, {
+  desc = "Unset global terminal color variables before changing colorscheme",
+  group = group,
+  callback = function()
+    for i = 0, 15 do
+      vim.g["terminal_color_" .. i] = nil
+    end
   end,
 })
 

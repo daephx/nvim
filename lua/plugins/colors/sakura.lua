1d2619a42dc6f2293babfef838a6b8fb7416ab05
daephx <daephxdev@gmail.com>
2023-11-01 08:23:06 -0700

    refactor: improve module/reference `config.colors`
    Reworks the colorscheme helper module to have better naming for
    functions, improve function definitions, fix minor bugs.
    
    - docs: reword autocmd and luadoc descriptions
    - docs: use triple dashes for luadoc comments
    
    - fix: add nil check for filepath in load function
    - fix: call colorscheme load autocmd on `VimEnter`
    - fix: default overrides not loaded if colorscheme not set
    - fix: update terminal colors on colorscheme event
      Terminal color are not updated when changing the active colorscheme.
      By changing the variables `g:terminal_color_{0..15}` and restarting
      the terminal buffer, we can get the correct colors.
    
    - refactor: define colors module augroup as variable
    - refactor: improve get highlight function
    - refactor: rename functions to shorthand names: more like `vim.api`
    - refactor!: remove runtime check for plugins/colors
      prefer using a local autocmd or the plugins builtin override options
    
    - feat: add func to register colorscheme override autocmd
      As stated in the relevant luadoc: This function extends the set_hl
      function by wrapping it in an autocmd so that colorschemes that don't
      provide their own override capabilities can still be easily modified.

--- lua/plugins/colors/sakura.lua
+++ lua/plugins/colors/sakura.lua
@@ -7,11 +7,14 @@ return {
   opts = {
     transparent = true,
   },
-  colors = {
-    NormalFloat = { link = "Normal" },
-    FloatBorder = { fg = "#6e6a86" },
-    CursorLineNr = { fg = "#f6c177" },
-    SpecialChar = { fg = "#56b1bb" },
-    String = { fg = "#cbaa73" },
-  },
+  init = function()
+    local colors = require("config.colors")
+    colors.set_hl_autocmd("sakura", {
+      NormalFloat = { link = "Normal" },
+      FloatBorder = { fg = "#6e6a86" },
+      CursorLineNr = { fg = "#f6c177" },
+      SpecialChar = { fg = "#56b1bb" },
+      String = { fg = "#cbaa73" },
+    })
+  end,
 }

b1008b2d52ceb0baf1a640a0e8de89e8e8fddc32
daephx <daephxdev@gmail.com>
2024-02-13 13:46:04 -0800

    feat(todo): add test keyword and update signs/highlights
    - Use default values for sign options enabled, priority
    - Add local definition for keyword TEST with preferred sign icon
    - Replace other sign icons with thicker variants
    - Override default highlight colors replacing link Identifier -> Include
      Other colors and fallbacks remain unchanged
    
    - Change pattern for keywords `TODO(author): body`
    
    There is still an open Issue/PR to expand this functionality in the
    plugin repository, but for now this just modifies the default pattern
    string to extend the highlight to these extra cases.

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -16,29 +16,36 @@ return {
     { "<leader>ft", "<cmd>TodoTelescope<CR>", desc = "Search Todo comments" },
   },
   opts = {
-    signs = true, -- show icons in the signs column
-    sign_priority = 8,
     keywords = {
       FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "FIX", "ISSUE" } },
-      HACK = { icon = " ", color = "warning" },
+      HACK = { icon = "󰈸 ", color = "warning" },
       NOTE = { icon = "󰍨 ", color = "hint", alt = { "INFO" } },
-      PERF = { icon = "󰅒 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      PERF = { icon = "󰓅 ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      TEST = { icon = "󰂓 ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
       TODO = { icon = " ", color = "info", alt = { "WIP" } },
       WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
     },
     highlight = {
       keyword = "fg", -- "fg", "bg", "wide" or empty.
       after = "", -- "fg" or "bg" or empty
-      pattern = [[.*<(KEYWORDS)(\(.*\))?:]],
-      comments_only = true,
+      pattern = [[(KEYWORDS)\s*(\([^\)]*\))?:]],
+      comments_only = true, -- uses treesitter to match keywords in comments only
       -- list of file types to exclude highlighting
       exclude = { "help", "log" },
+    },
+    colors = {
+      default = { "Include", "#7C3AED" },
+      error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+      hint = { "DiagnosticHint", "#10B981" },
+      info = { "DiagnosticInfo", "#2563EB" },
+      test = { "Include", "#FF00FF" },
+      warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
     },
     search = {
       -- regex that will be used to match keywords.
       -- don't replace the (KEYWORDS) placeholder!
       -- Match to colon ':', optional parenthesis | KEYWORD(AUTHOR):
-      pattern = [[\b(KEYWORDS)(\(.*\))?:]],
+      pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]],
     },
   },
   config = function(_, opts)

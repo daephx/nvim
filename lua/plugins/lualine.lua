bf8348f7d0e608de458ccf0ae0d792b07c2370a5
daephx <daephxdev@gmail.com>
2024-02-15 10:54:11 -0800

    fix(lualine): improve display names for special windows
    Changes the labels that are displayed for filetypes buffers such as
    NvimTree, mason, lazy.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -100,8 +100,12 @@ local windows = {
   "windows",
   use_mode_colors = true,
   filetype_names = {
+    NvimTree = "NvimTree",
+    checkhealth = "checkhealth",
     fugitive = "Fugitive",
     lazy = "Lazy",
+    mason = "Mason",
+    tsplayground = "TSPlayground",
   },
 }
 

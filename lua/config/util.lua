0a28bbeed3d007a730a3f99899ed1a8fadd04f8e
daephx <daephxdev@gmail.com>
2024-02-20 15:00:40 -0800

    feat: add util wrapper register_keymaps
    Allows keymaps to be applied by using a list of keymaps with two tables.
    One for options to apply to all keymaps and the second for the keymaps.
    
    The benefit of using this wrapper is that each keymap can also accept
    options to be merged with the first set of defaults. This way you can
    set the whole list of keymaps to be `silent=true` but each keymap can
    have either own set of options to overwrite the supplied defaults.
    
    Example:
    
    ```lua
    -- Usage of the util function:
    local default_opts = { buffer = 0, silent = true }
    require("config.util").register_keymaps(default_opts, {
      { "n", "<leader>a", callback, { silent = false, desc = "action: a" } }
      { "n", "<leader>b", callback, { expr   = true,  desc = "action: b" } }
      { "n", "<leader>b", callback, { remap  = true,  desc = "action: c" } }
    })
    
    -- Resulting table of keymaps:
    -- { "n", "<leader>a", callback, { buffer = 0, silent = false,               desc = "action: a" } }
    -- { "n", "<leader>b", callback, { buffer = 0, silent = true,  expr  = true, desc = "action: b" } }
    -- { "n", "<leader>b", callback, { buffer = 0, silent = true,  remap = true, desc = "action: c" } }
    ```

--- lua/config/util.lua
+++ lua/config/util.lua
@@ -1,4 +1,17 @@
+-- Neovim utility functions to be used across config files.
+
 local M = {}
+
+---Wrapper to extend options for various keymap definitions
+---@param options table|nil default options for keymaps, assign `nil` to use defaults.
+---@param keymaps table list of keymap definitions to be applied
+function M.register_keymaps(options, keymaps)
+  for _, map in pairs(keymaps) do
+    local mode, lhs, rhs, opts = unpack(map)
+    opts = vim.tbl_extend("force", options or {}, opts or {})
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
 
 ---Define how folded lines should be displayed
 ---@return string

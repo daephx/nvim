38eb949395202bad2f5726a33b186cf4014b5cfa
daephx <daephxdev@gmail.com>
2024-06-09 12:53:50 -0700

    fix(lsp): Correct handling of code lens autocmd
    This commit addresses an issue where error messages would occur on the
    CursorHold event, specifically when a buffer had an attached code lens
    capable client. The error messages were triggered by unsupported methods
    for code lens.
    
    - Adjusted the autocmd to properly handle code lens refresh.
    - Ensured that the autocmd is triggered correctly without causing errors.
    - Moved group definition for clarity and to clear event on subsequent calls.

--- lua/plugins/lsp/handlers/codelens.lua
+++ lua/plugins/lsp/handlers/codelens.lua
@@ -1,18 +1,19 @@
 local M = {}
 
--- Enable codelens
+---Register autocmd for LSP codelens
 ---@param client table
 ---@param bufnr integer
 M.enable_code_lens = function(client, bufnr)
+  local group = vim.api.nvim_create_augroup("LspCodeLens", {})
   if client.supports_method("textDocument/codeLens") then
-    local augroup = vim.api.nvim_create_augroup("LspCodeLens", {})
-    vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave", "BufWritePost", "CursorHold" }, {
-      desc = "Refresh codelens for current buffer",
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "CursorHold", "InsertLeave" }, {
       buffer = bufnr,
-      group = augroup,
-      callback = vim.lsp.codelens.refresh,
+      desc = "Refresh codelens for current buffer",
+      group = group,
+      callback = function(ev)
+        vim.lsp.codelens.refresh({ bufnr = ev.buf })
+      end,
     })
-    vim.schedule(vim.lsp.codelens.refresh)
   end
 end
 

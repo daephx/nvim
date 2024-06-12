e3efd548eca9c585bc86aea655a6c1324d368c5a
daephx <daephxdev@gmail.com>
2024-06-12 14:59:31 -0700

    refactor(plugin): replace colorizer.nvim with nvim-highlight-colors
    Replaced colorizer.nvim with nvim-highlight-colors for improved color
    highlighting support. Ensures better handling of various color formats
    and integrates seamlessly with LSP.

---
+++ lua/plugins/highlight-colors.lua
@@ -0,0 +1,25 @@
+-- nvim-highlight-colors | Highlight colors for neovim
+-- https://github.com/brenoprata10/nvim-highlight-colors
+
+---@type LazyPluginSpec
+return {
+  "brenoprata10/nvim-highlight-colors",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    render = "background",
+    enable_hex = true,
+    enable_short_hex = true,
+    enable_rgb = true,
+    enable_hsl = true,
+    enable_var_usage = true,
+    enable_named_colors = false,
+    enable_tailwind = true,
+    custom_colors = {},
+    exclude_filetypes = {},
+    exclude_buftypes = {
+      "nofile",
+      "popup",
+      "prompt",
+    },
+  },
+}

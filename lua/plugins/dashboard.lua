8f592eb6211f4f6257b67dbbacb322cc8d8c7e34
daephx <daephxdev@gmail.com>
2024-07-06 02:53:13 -0700

    fix(dashboard): add keymap doc to dashboard: `quit`

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -71,7 +71,7 @@ return {
         {
           icon = "",
           desc = "Exit",
-          keymap = "",
+          keymap = "SHFT z z",
           key = "q",
           action = "exit",
         },

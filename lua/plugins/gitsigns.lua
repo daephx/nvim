4d53e7e4210f5889cfa36e8c4213d812c7594c65
daephx <daephxdev@gmail.com>
2024-07-18 02:28:45 -0700

    fix(gitsigns): disable staged signs to avoid duplication issues
    The latest Gitsigns update introduces additional signs for staged hunks.
    However, the current implementation often results in duplicate signs
    when modifying already staged sections, likely due to Neovim signcolumn
    prioritization limitations.
    
    To mitigate this, the staged signs functionality has been disabled using
    the `signs_staged_enable` boolean configuration. Custom signs for
    various actions remain locally defined to ensure consistent visual cues.
    
    Additionally, the previously missing `untracked` sign definition has
    been added to both the regular and staged sign configurations.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -13,7 +13,17 @@ return {
       delete = { text = "▎" },
       topdelete = { text = "▎" },
       changedelete = { text = "▎" },
+      untracked = { text = "▎" },
     },
+    signs_staged = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "▎" },
+      topdelete = { text = "▎" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    signs_staged_enable = false,
     current_line_blame = true,
     current_line_blame_formatter_opts = {
       relative_time = false,

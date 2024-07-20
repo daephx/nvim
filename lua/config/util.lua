c2a445c03f21fbd0dd8fed79525b29ccd150bcf8
daephx <daephxdev@gmail.com>
2024-07-20 00:28:08 -0700

    feat: use empty foldtext to enable fold highlights
    By removing our custom foldtext function located in `lua/config/util`
    and setting the option `vim.opt.foldtext` to an empty string, internally
    neovim will handle highlighting the folded region for us using the
    already present treesitter highlights.
    
    After the revert/removal of the `vim.treesitter.foldtext()` function in
    neovim core, another option was added to allow similar but limited
    functionality by setting the foldtext option to that of an empty string,
    since that value would be otherwise invalid anyway.
    
    More work needs to be done to proved a more robust folding system.
    Many people will utilize plugins like ufo.nvim but I wish to have the
    most simple implementation until better support is provided by neovim
    core.

--- lua/config/util.lua
+++ lua/config/util.lua
@@ -13,21 +13,4 @@ function M.register_keymaps(options, keymaps)
   end
 end
 
----Define how folded lines should be displayed
----@return string
-function M.foldtext()
-  local foldstart = vim.fn.getline(vim.v.foldstart)
-  local foldend = vim.fn.getline(vim.v.foldend)
-  local linecount = vim.v.foldend - vim.v.foldstart + 1
-  local tabwidth = string.rep(" ", vim.bo.tabstop)
-  return string.format(
-    "%s … %s %s",
-    ---@cast foldstart string
-    vim.fn.substitute(foldstart, "\t", tabwidth, "g"),
-    ---@cast foldend string
-    vim.fn.trim(foldend),
-    string.format("[%s lines]", linecount)
-  )
-end
-
 return M

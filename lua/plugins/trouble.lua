46cc54a6fcbb5ade02219250ba99b042bf73a8e5
daephx <daephxdev@gmail.com>
2024-07-20 01:02:46 -0700

    chore(trouble): update trouble.nvim config to v3
    - Removed deprecated and unused settings.
    - Added default keymaps to the lazy plugin spec.
    - Added comments for plugin dependencies.
    - Included `@module` for proper type definitions with `lazydev/lua-ls`.
    - Disabled auto-focus; now requires manual selection or Enter key.
    - Disabled status column in trouble buffers.
    - Ensured user-defined window border is applied correctly.
    - Moved buffer autocmd to the lazy spec initialization function.

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,32 +1,38 @@
 -- trouble.nvim | pretty diagnostics, references, telescope results
 -- https://github.com/folke/trouble.nvim
+---@module "trouble"
 
 ---@type LazyPluginSpec
 return {
   "folke/trouble.nvim",
-  cmd = { "TroubleToggle", "Trouble" },
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    padding = false, -- add an extra new line on top of the list
-    auto_open = false, -- automatically open the list when you have diagnostics
-    auto_close = true, -- automatically close the list when you have no diagnostics
-    use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
+  cmd = { "Trouble" },
+  dependencies = {
+    -- Lua fork of vim-web-devicons for neovim
+    { "nvim-tree/nvim-web-devicons" },
   },
-  config = function(_, opts)
-    require("trouble").setup(opts)
-
-    --- Autocmds ---
-
-    vim.api.nvim_create_augroup("TroubleBuffer", {})
-    vim.api.nvim_create_autocmd("FileType", {
-      desc = "Apply local options to Trouble buffer",
-      group = "TroubleBuffer",
-      pattern = "Trouble",
-      callback = function()
-        vim.opt_local.cursorline = true
-        vim.opt_local.cursorlineopt = "both"
-        vim.opt_local.colorcolumn = "0"
-      end,
-    })
-  end,
+  -- stylua: ignore
+  keys = {
+    { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP (Trouble)" },
+    { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+    { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+    { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+  },
+  ---@type trouble.Config
+  opts = {
+    auto_close = false, -- Auto close when there are no items
+    focus = true, -- Focus the window when opened
+    follow = false, -- Follow the current item
+    win = {
+      border = vim.g.border,
+      wo = {
+        number = false,
+        relativenumber = false,
+        colorcolumn = "0",
+        cursorline = true,
+        cursorlineopt = "both",
+      },
+    },
+  },
 }

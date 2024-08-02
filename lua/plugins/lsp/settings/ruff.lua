901836fb5694b6759ff682d929395ae9db264f9b
daephx <daephxdev@gmail.com>
2024-08-02 06:53:41 -0700

    refactor(lsp): replace ruff_lsp with ruff server
    Recent versions of Ruff include a built-in LSP implementation via the
    `ruff server` command. This replaces the previously used `ruff_lsp`
    project, which was a separate wrapper for Ruff with an LSP interface.
    
    - Replaced ruff_lsp with ruff in mason ensure_installed
    - Added server settings for ruff
    - Added autocmd to disable ruff hover capabilities
      This allows pyright to provide hover capabilities but perhaps we could
      make this dependent on if pyright is available/configured or attached
      to the buffer? The latter possibly depending on lsp load order.

---
+++ lua/plugins/lsp/settings/ruff.lua
@@ -0,0 +1,26 @@
+-- ruff: Ruff Language-server configuration
+-- https://github.com/astral-sh/ruff
+---@module "lspconfig"
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "LSP: Disable hover capability from Ruff",
+  group = vim.api.nvim_create_augroup("LspAttach#Ruff#DisableHover", {}),
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client ~= nil and client["name"] == "ruff" then
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+})
+
+---@type lspconfig.Config
+return {
+  init_options = {
+    settings = {
+      lineLength = 100,
+      quote_style = "double",
+      indent_style = "space",
+      docstring_code_format = true,
+    },
+  },
+}

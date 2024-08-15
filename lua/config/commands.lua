07b5f876e1c3892ddb9e56bb8e551be125d7248f
daephx <daephxdev@gmail.com>
2024-08-15 07:52:25 -0700

    feat: add user command to redirect output to a new buffer
    Added the `Redir` user command, which redirects the output of a given
    command to a new buffer. For example, `Redir lua = require("vim.lsp")`
    creates a modifiable, searchable buffer with the command's output.
    This is useful when the `messages` command falls short due to its lack
    of searchability and limited accessibility for larger outputs.

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -57,3 +57,20 @@ end, {
     return modules
   end,
 })
+
+---Redirect the output of a command to a new buffer
+---@param ctx table
+local redirect_output = function(ctx)
+  local exec = vim.api.nvim_exec2(ctx.args, { output = true })
+  local lines = vim.split(exec.output, "\n", { plain = true })
+  vim.cmd.new()
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.spell = false
+  vim.opt_local.modified = false
+end
+
+-- Register commands: Redir
+vim.api.nvim_create_user_command("Redir", redirect_output, {
+  complete = "command",
+  nargs = "+",
+})

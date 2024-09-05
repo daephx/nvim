3ec46397563a92889d026b6409efd318a24b72ae
daephx <daephxdev@gmail.com>
2024-09-05 08:55:21 -0700

    refactor(mason): move LSP and DAP handlers to mason module
    - Moved LSP and DAP handlers to mason module.
    - Removed event from mason-nvim-dap to allow auto-install on startup.
    - Removed `runtime!` loading for dap/adapters modules.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -39,6 +39,33 @@ return {
         "taplo",
         "yamlls",
       },
+      handlers = {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        ---@param server_name string
+        function(server_name)
+          -- Define default variables
+          local server_mpath = "plugins/lsp/settings"
+          local default_options = {
+            capabilities = require("plugins.lsp.capabilities"),
+            on_attach = require("plugins.lsp.attach"),
+          }
+          local namespace = table.concat({ server_mpath, server_name }, "/")
+          local _, module = pcall(require, namespace)
+          module = type(module) == "table" and module or {}
+          local config = vim.tbl_deep_extend("force", default_options, module or {})
+          require("lspconfig")[server_name].setup(config)
+        end,
+
+        ["rust_analyzer"] = function()
+          require("plugins/lsp/settings/rust_analyzer")
+        end,
+
+        ["jdtls"] = function()
+          require("plugins/lsp/settings/jdtls")
+        end,
+      },
     },
   },
   {
@@ -66,6 +93,16 @@ return {
       automatic_installation = false,
       automatic_setup = true,
       ensure_installed = {},
+      handlers = {
+        function(config)
+          local adapters_path = "plugins/dap/adapters"
+          local namespace = table.concat({ adapters_path, config.name }, "/")
+          local _, module = pcall(require, namespace)
+          module = type(module) == "table" and module or {}
+          config = vim.tbl_deep_extend("force", config, module or {})
+          require("mason-nvim-dap").default_setup(config)
+        end,
+      },
     },
   },
 }

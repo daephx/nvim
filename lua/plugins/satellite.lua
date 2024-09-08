d0a94b539d3e95f757d8476cbffebb341eccb981
daephx <daephxdev@gmail.com>
2024-09-08 11:05:18 -0700

    fix(satellite): add default highlight for SatelliteBar
    - Added autocmd to apply highlights for satellite.nvim on ColorScheme change.
    - Added default highlight for SatelliteBar with links to 'Pmenu'.
    - Disable 'winblend' to prevent black background due to a known Neovim core bug.

--- lua/plugins/satellite.lua
+++ lua/plugins/satellite.lua
@@ -6,5 +6,16 @@
 return {
   "lewis6991/satellite.nvim",
   event = { "BufReadPost", "BufNewFile" },
-  config = true,
+  init = function()
+    -- Apply highlight group values for plugin on ColorScheme change.
+    local colors = require("config.colors")
+    colors.set_hl_autocmd(nil, {
+      SatelliteBar = { link = "Pmenu", default = true },
+    })
+  end,
+  opts = {
+    -- FIX: Disable winblend to prevent black background
+    -- https://github.com/neovim/neovim/issues/18576
+    winblend = 0,
+  },
 }

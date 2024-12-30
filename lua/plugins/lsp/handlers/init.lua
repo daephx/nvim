fea6fd344989ec8762d4ec176eeb6ae60e66fd0a
daephx <daephxdev@gmail.com>
2024-12-30 07:16:49 -0800

    fix(lsp): disable custom handlers for Neovim 0.11+
    - Skip custom LSP handlers in Neovim 0.11 and newer.
    - Retain support for Neovim 0.10 with custom hover and signature help handlers.
    - Comment out and remove broken "goto definition" handler for split window.
      - This feature doesn't function as expected and may be revisited later.

--- lua/plugins/lsp/handlers/init.lua
+++ lua/plugins/lsp/handlers/init.lua
@@ -1,15 +1,25 @@
--- Define LSP handlers to be exported globally
+-- Define custom LSP handlers with global export
+-- TODO: handlers are deprecated and should be removed in Neovim 0.13.
 
+-- Skip loading in newer versions of Neovim (0.11+).
+if vim.fn.has("nvim-0.11") == 1 then
+  return
+end
+
+-- FIXME: Custom "goto definition" handler for split window not working.
+-- local definition = require("plugins.lsp.handlers.definition")
+-- vim.lsp.handlers["textDocument/definition"] = definition.goto_definition
+
+-- Hover handler with custom border style
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = vim.g.border,
+})
+
+-- NOTE: Doesn't seem to work with signatureHelp plugin
+-- Signature help handler with custom border and close events
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
   border = vim.g.border,
   close_events = { "BufHidden", "CursorMoved", "CursorMovedI", "InsertCharPre" },
   focusable = false,
   silent = true,
 })
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = vim.g.border,
-})
-
-local definition = require("plugins.lsp.handlers.definition")
-vim.lsp.handlers["textDocument/definition"] = definition.goto_definition

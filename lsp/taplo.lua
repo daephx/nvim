e552973a41321bf75b008f0aa3c2b7479cc1344e
daephx <daephxdev@gmail.com>
2026-04-18 01:20:26 -0700

    refactor(lsp): use builtin fs.joinpath for taplo cache

--- lsp/taplo.lua
+++ lsp/taplo.lua
@@ -1,11 +1,14 @@
 -- taplo: A TOML toolkit written in Rust
 -- https://github.com/tamasfe/taplo
+local nvim_cache = vim.fn.stdpath("cache")
+local taplo_cache = vim.fs.joinpath(nvim_cache, "taplo")
 
+-- Set up taplo
 ---@type vim.lsp.Config
 return {
   settings = {
     evenBetterToml = {
-      cachePath = vim.fn.stdpath("cache") .. "/taplo",
+      cachePath = taplo_cache,
       schema = {
         enabled = true,
         repositoryEnabled = true,

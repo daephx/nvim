cf17bab5d01ffc0776848a0376d977e779c8d7ff
daephx <daephxdev@gmail.com>
2026-05-19 05:21:58 -0700

    refactor(dropbar): migrate keymaps to lazy.nvim keys spec

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -5,23 +5,17 @@
 return {
   "Bekaboo/dropbar.nvim",
   event = { "BufNewFile", "BufReadPost" },
-  dependencies = {
-    -- optional, but required for fuzzy finder support
-    -- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  -- stylua: ignore
+  keys = {
+    { "<leader>;", function() require("dropbar.api").pick() end, desc = "Pick symbols in winbar" },
+    { "[;", function() require("dropbar.api").goto_context_start() end, desc = "Go to start of current context" },
+    { "];", function() require("dropbar.api").select_next_context() end, desc = "Select next context" },
   },
   ---@module "dropbar"
+  ---@type dropbar_opts_t
   opts = {
     kinds = {
       symbols = require("config.icons").kinds,
     },
   },
-  config = function(_, opts)
-    require("dropbar").setup(opts)
-    local dropbar_api = require("dropbar.api")
-    require("config.util").register_keymaps(nil, {
-      { "n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" } },
-      { "n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" } },
-      { "n", "];", dropbar_api.select_next_context, { desc = "Select next context" } },
-    })
-  end,
 }

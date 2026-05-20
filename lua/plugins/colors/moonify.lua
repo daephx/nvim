c01b3f7fa53be631194451c80abfd90366d0e4d2
daephx <daephxdev@gmail.com>
2026-05-19 17:30:09 -0700

    refactor(colors): simplify moonfly theme configuration
    - Remove custom terminal colors, use theme defaults
    - Remove explicit rainbow-delimiters overrides
    - Add Pmenu highlight linked to NormalFloat

--- lua/plugins/colors/moonify.lua
+++ lua/plugins/colors/moonify.lua
@@ -1,25 +1,6 @@
 --  vim-moonfly-colors | A dark charcoal theme for modern Neovim & classic Vim
 -- https://github.com/bluz71/vim-moonfly-colors
 
-local terminal = {
-  "#141414",
-  "#ff5454",
-  "#8cc85f",
-  "#e3c78a",
-  "#80a0ff",
-  "#cf87e8",
-  "#79dac8",
-  "#c6c6c6",
-  "#474747",
-  "#ff5189",
-  "#36c692",
-  "#c6c684",
-  "#74b2ff",
-  "#ae81ff",
-  "#85dc85",
-  "#e4e4e4",
-}
-
 ---@type LazySpec
 return {
   "bluz71/vim-moonfly-colors",
@@ -38,7 +19,6 @@ return {
     local palette = require("moonfly").palette
     local colors = require("config.colors")
     colors.set_hl_autocmd("moonfly", {
-      -- General
       CursorLineNr = { fg = palette.blue, bold = true },
       CursorLineSign = { link = "Normal" },
       FloatBorder = { fg = palette.grey39 },
@@ -46,6 +26,7 @@ return {
       Folded = { bg = palette.grey18, fg = palette.grey50 },
       MsgSeparator = { link = "WinSeparator" },
       NormalFloat = { fg = "#c6c6c6", bg = "#080808" },
+      Pmenu = { link = "NormalFloat" },
       Underlined = { fg = palette.purple, underline = true },
       VertSplit = { link = "WinSeparator" },
       Visual = { bg = palette.bay },
@@ -68,27 +49,6 @@ return {
       -- Treesitter
       TreesitterContextBottom = {},
       ["@string.special.url"] = { link = "Underlined" },
-
-      -- Treesitter Rainbow
-      RainbowDelimiterBlue = { fg = palette.blue },
-      RainbowDelimiterCyan = { fg = palette.sky },
-      RainbowDelimiterGreen = { fg = palette.green },
-      RainbowDelimiterOrange = { fg = palette.orange },
-      RainbowDelimiterRed = { fg = palette.red },
-      RainbowDelimiterViolet = { fg = palette.violet },
-      RainbowDelimiterYellow = { fg = palette.yellow },
-    })
-  end,
-  init = function()
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      desc = "Replace terminal colors for colorscheme: moonfly",
-      group = vim.api.nvim_create_augroup("ColorScheme#MoonflyTerminal", { clear = true }),
-      pattern = "moonfly",
-      callback = function()
-        for i, color in ipairs(terminal) do
-          vim.g["terminal_color_" .. i - 1] = color
-        end
-      end,
     })
   end,
 }
